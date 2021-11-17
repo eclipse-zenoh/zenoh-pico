19c4ff95d75887d63d7440fb8237f688bad0b775
Luca Cominardi <luca.cominardi@gmail.com>
2021-11-17 11:59:56 +0100

    Remove duplicate function definition

--- include/zenoh-pico/transport/manager.h
+++ include/zenoh-pico/transport/manager.h
@@ -15,23 +15,4 @@
 #ifndef ZENOH_PICO_TRANSPORT_MANAGER_H
 #define ZENOH_PICO_TRANSPORT_MANAGER_H
 
-#include "zenoh-pico/utils/collections.h"
-#include "zenoh-pico/session/session.h"
-#include "zenoh-pico/session/session.h"
-#include "zenoh-pico/protocol/core.h"
-#include "zenoh-pico/protocol/msg.h"
-#include "zenoh-pico/protocol/msgcodec.h"
-
-/*------------------ SN helpers ------------------*/
-int _zn_sn_precedes(z_zint_t sn_resolution_half, z_zint_t sn_left, z_zint_t sn_right);
-
-/*------------------ Transmission and Reception helpers ------------------*/
-int _zn_send_t_msg(zn_session_t *zn, _zn_transport_message_t *m);
-int _zn_send_z_msg(zn_session_t *zn, _zn_zenoh_message_t *m, zn_reliability_t reliability, zn_congestion_control_t cong_ctrl);
-
-_zn_transport_message_p_result_t _zn_recv_t_msg(zn_session_t *zn);
-void _zn_recv_t_msg_na(zn_session_t *zn, _zn_transport_message_p_result_t *r);
-
-int _zn_handle_transport_message(zn_session_t *zn, _zn_transport_message_t *msg);
-
 #endif /* ZENOH_PICO_TRANSPORT_MANAGER_H */
