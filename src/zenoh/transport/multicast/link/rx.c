8fa94b4976d7b84f77754c9498fc2e7103eda645
Carlos Guimarães <carlos.em.guimaraes@gmail.com>
2021-12-01 12:48:07 +0100

    Handle close message in transport multicast

--- src/zenoh/transport/multicast/link/rx.c
+++ src/zenoh/transport/multicast/link/rx.c
@@ -223,6 +223,17 @@ int _zn_multicast_handle_transport_message(_zn_transport_multicast_t *ztm, _zn_t
     case _ZN_MID_CLOSE:
     {
         _Z_DEBUG("Closing session as requested by the remote peer");
+
+        _zn_transport_peer_entry_t *entry = _zn_find_peer_entry(ztm->peers, addr);
+        if(_ZN_HAS_FLAG(t_msg->header, _ZN_FLAG_T_I))
+        {
+            // Check if the Peer ID matches the remote address in the knonw peer list
+            if (entry->remote_pid.len != t_msg->body.close.pid.len
+              || memcmp(entry->remote_pid.val, t_msg->body.close.pid.val, entry->remote_pid.len) != 0)
+                return _z_res_t_OK;
+        }
+        ztm->peers = _zn_transport_peer_entry_list_drop_filter(ztm->peers, _zn_transport_peer_entry_cmp, entry);
+
         return _z_res_t_OK;
     }
 
