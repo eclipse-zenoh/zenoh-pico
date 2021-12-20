3689f723bb85b27139500890a64f6faa6af323e6
Carlos Guimaraes <carlos.em.guimaraes@gmail.com>
2021-12-20 22:24:40 +0100

    Pass on the memory management for multicast transport

--- src/transport/multicast/link/task/read.c
+++ src/transport/multicast/link/task/read.c
@@ -107,7 +107,10 @@ void *_znp_multicast_read_task(void *arg)
                 int res = _zn_multicast_handle_transport_message(ztm, &r.value.transport_message, &addr);
 
                 if (res == _z_res_t_OK)
+                {
                     _zn_t_msg_clear(&r.value.transport_message);
+                    _z_bytes_clear(&addr);
+                }
                 else
                     goto EXIT_RECV_LOOP;
             }
