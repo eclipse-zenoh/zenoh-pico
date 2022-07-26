7fd429253bc6970d8a2913edf97d038a15948937
Carlos Guimarães <carlos.em.guimaraes@gmail.com>
2022-07-26 10:46:24 +0200

    Fix keyexpr on query reply examples

--- examples/c11/z_queryable.c
+++ examples/c11/z_queryable.c
@@ -26,8 +26,8 @@ void query_handler(z_query_t *query, void *ctx)
     (void) (ctx);
     const char *res = z_keyexpr_to_string(z_query_keyexpr(query));
     z_bytes_t pred = z_query_value_selector(query);
-    printf(">> [Queryable ] Received Query '%s%.*s'\n", res, (int)pred.len, pred.start);
-    z_query_reply(query, z_query_keyexpr(query), (const unsigned char *)value, strlen(value));
+    printf(" >> [Queryable handler] Received Query '%s%.*s'\n", res, (int)pred.len, pred.start);
+    z_query_reply(query, z_keyexpr(keyexpr), (const unsigned char *)value, strlen(value));
 }
 
 int main(int argc, char **argv)
