413407ccdda0edd80ccd20fd03fca024c114671b
Carlos Guimarães <carlos.em.guimaraes@gmail.com>
2022-08-10 11:47:28 +0200

    Finish API documentation

--- docs/api.rst
+++ docs/api.rst
@@ -16,8 +16,11 @@
 API Reference
 *************
 
+Zenoh Types
+-----------
+
 Enums
------
+~~~~~
 
 .. autocenum:: constants.h::z_whatami_t
 .. autocenum:: constants.h::z_keyexpr_canon_status_t
@@ -33,8 +36,8 @@ Enums
 .. autocenum:: constants.h::z_submode_t
 .. autocenum:: constants.h::z_query_target_t
 
-Types
------
+Data Structures
+~~~~~~~~~~~~~~~
 
 .. autoctype:: types.h::z_zint_t
 .. autoctype:: types.h::z_bytes_t
@@ -48,7 +51,6 @@ Types
 .. autoctype:: types.h::z_publisher_t
 .. autoctype:: types.h::z_queryable_t
 .. autoctype:: types.h::z_encoding_t
-.. autoctype:: types.h::z_period_t
 .. autoctype:: types.h::z_value_t
 .. autoctype:: types.h::z_subscriber_options_t
 .. autoctype:: types.h::z_pull_subscriber_options_t
@@ -66,8 +68,40 @@ Types
 .. autoctype:: types.h::z_reply_t
 .. autoctype:: types.h::z_reply_data_t
 
+Arrays
+~~~~~~
+.. c:type:: z_str_array_t
+
+  Represents an array of ``char *``.
+
+  Operations over :c:type:`z_str_array_t` must be done using the provided functions:
+
+    - ``char *z_str_array_get(z_str_array_t *a, size_t k);``
+    - ``size_t z_str_array_len(z_str_array_t *a);``
+    - ``uint8_t z_str_array_array_is_empty(z_str_array_t *a);``
+
+.. c:type:: z_hello_array_t
+
+  Represents an array of ``z_hello_t``.
+
+  Operations over :c:type:`z_hello_array_t` must be done using the provided functions:
+
+    - ``z_hello_t *z_hello_array_get(z_hello_array_t *a, size_t k);``
+    - ``size_t z_hello_array_len(z_hello_array_t *a);``
+    - ``uint8_t z_hello_array_array_is_empty(z_hello_array_t *a);``
+
+.. c:type:: z_reply_data_array_t
+
+  Represents an array of ``z_reply_data_t``.
+
+  Operations over :c:type:`z_reply_data_array_t` must be done using the provided functions:
+
+    - ``z_reply_data_t *z_reply_data_array_get(z_reply_data_array_t *a, size_t k);``
+    - ``size_t z_reply_data_array_len(z_reply_data_array_t *a);``
+    - ``uint8_t z_reply_data_array_array_is_empty(z_reply_data_array_t *a);``
+
 Owned Types
------------
+~~~~~~~~~~~
 
 Like most ``z_owned_X_t`` types, you may obtain an instance of ``z_X_t`` by loaning it using ``z_X_loan(&val)``.
 The ``z_loan(val)`` macro, available if your compiler supports C11's ``_Generic``, is equivalent to writing ``z_X_loan(&val)``.
@@ -98,10 +132,6 @@ To check if ``val`` is still valid, you may use ``z_X_check(&val)`` or ``z_check
 
   A zenoh-allocated :c:type:`z_session_t`.
 
-.. c:type:: z_owned_info_t
-
-  A zenoh-allocated :c:type:`z_info_t`.
-
 .. c:type:: z_owned_subscriber_t
 
   A zenoh-allocated :c:type:`z_subscriber_t`.
@@ -122,10 +152,6 @@ To check if ``val`` is still valid, you may use ``z_X_check(&val)`` or ``z_check
 
   A zenoh-allocated :c:type:`z_encoding_t`.
 
-.. c:type:: z_owned_period_t
-
-  A zenoh-allocated :c:type:`z_period_t`.
-
 .. c:type:: z_owned_consolidation_strategy_t
 
   A zenoh-allocated :c:type:`z_consolidation_strategy_t`.
@@ -138,13 +164,13 @@ To check if ``val`` is still valid, you may use ``z_X_check(&val)`` or ``z_check
 
   A zenoh-allocated :c:type:`z_query_consolidation_t`.
 
-.. c:type:: z_owned_reply_t
-
-  A zenoh-allocated :c:type:`z_reply_t`.
-
 .. c:type:: z_owned_reply_data_t
 
   A zenoh-allocated :c:type:`z_reply_data_t`.
+
+.. c:type:: z_owned_reply_t
+
+  A zenoh-allocated :c:type:`z_reply_t`.
 
 .. c:type:: z_owned_str_array_t
 
@@ -159,7 +185,7 @@ To check if ``val`` is still valid, you may use ``z_X_check(&val)`` or ``z_check
   A zenoh-allocated :c:type:`z_reply_data_array_t`.
 
 Closures
---------
+~~~~~~~~
 
 A closure is a structure that contains all the elements for stateful, memory-leak-free callbacks:
   - context: a pointer to an arbitrary state.
@@ -179,8 +205,12 @@ It is guaranteed that:
 .. autoctype:: types.h::z_owned_closure_reply_t
 .. autoctype:: types.h::z_owned_closure_zid_t
 
+
+Zenoh Functions
+---------------
+
 Macros
-------
+~~~~~~
 .. autocmacro:: macros.h::z_loan
 .. autocmacro:: macros.h::z_move
 .. autocmacro:: macros.h::z_check
@@ -188,241 +218,8 @@ Macros
 .. autocmacro:: macros.h::z_drop
 .. autocmacro:: macros.h::z_closure
 
-.. Collections
-.. -----------
-
-.. Arrays
-.. ~~~~~~
-
-.. .. c:type:: struct name_array_t
-
-..   An array of elemets of type *name_t*.
-
-..   .. c:member:: unsigned int length
-
-..     The length of the array.
-
-..   .. c:member:: name_t *elem
-
-..     The *name_t* values.
-
-
-.. .. c:type:: struct name_p_array_t
-
-..   An array of pointers to elemets of type *name_t*.
-
-..   .. c:member:: unsigned int length
-
-..     The length of the array.
-
-..   .. c:member:: name_t *elem
-
-..     The pointers to the *name_t* values.
-
-
-.. Vectors
-.. ~~~~~~~
-
-.. .. c:type:: struct z_vec_t
-
-..   A sequence container that encapsulates a dynamic size array of pointers.
-
-..   .. c:member:: unsigned int _capacity
-
-..     The maximum capacity of the vector.
-
-..   .. c:member:: unsigned int _length
-
-..     The current length of the vector.
-
-..   .. c:member:: void **_elem
-
-..     The pointers to the values.
-
-.. .. c:function:: z_vec_t z_vec_make(unsigned int capacity)
-
-..   Initialize a :c:type:`z_vec_t` with a :c:member:`z_vec_t.capacity` of **capacity**,
-..   a :c:member:`z_vec_t.length` of **0** and a :c:member:`z_vec_t._elem` pointing to a
-..   newly allocated array of **capacity** pointers.
-
-.. .. c:function:: unsigned int z_vec_len(const z_vec_t* v)
-
-..   Return the current length of the given :c:type:z_vec_t.
-
-.. .. c:function:: void z_vec_append(z_vec_t* v, void* e)
-
-..   Append the element **e** to the vector **v** and take ownership of the appended element.
-
-.. .. c:function:: void z_vec_set(z_vec_t* sv, unsigned int i, void* e)
-
-..   Set the element **e** in the vector **v** at index **i** and take ownership of the element.
-
-.. .. c:function:: const void* z_vec_get(const z_vec_t* v, unsigned int i)
-
-..   Return the element at index **i** in vector **v**.
-
-
-.. Data Structures
-.. ---------------
-
-.. .. c:type:: struct _z_keyexpr_t
-
-..   Data structure representing a resource key.
-
-..   .. c:member:: int kind
-
-..     One of the following kinds:
-
-..       | ``Z_INT_RES_KEY``
-..       | ``Z_STR_RES_KEY``
-
-..   .. c:member:: union _z_keyexpr_t key
-
-..     .. c:member:: _z_zint_t rid
-
-..       A resource id (integer) when :c:member:`_z_keyexpr_t.kind` equals ``Z_INT_RES_KEY``.
-
-..     .. c:member:: char *rname
-
-..       A resource name (string) when :c:member:`_z_keyexpr_t.kind` equals ``Z_STR_RES_KEY``.
-
-.. .. c:type:: struct _z_subinfo_t
-
-..   Data structure representing a subscription mode (see :c:func:`_z_declare_subscriber`).
-
-..   .. c:member:: uint8_t kind
-
-..     One of the following subscription modes:
-
-..       | ``Z_PUSH_MODE``
-..       | ``Z_PULL_MODE``
-..       | ``Z_PERIODIC_PUSH_MODE``
-..       | ``Z_PERIODIC_PULL_MODE``
-
-..   .. c:member:: _z_period_t tprop
-
-..     The period. *Unsupported*
-
-.. .. c:type:: struct _z_timestamp_t
-
-..   Data structure representing a unique timestamp.
-
-..   .. c:member:: _z_zint_t time
-
-..     The time as a 64-bit long, where:
-
-..         - The higher 32-bit represent the number of seconds since midnight, January 1, 1970 UTC
-..         - The lower 32-bit represent a fraction of 1 second.
-
-..   .. c:member:: uint8_t clock_id[16]
-
-..     The unique identifier of the clock that generated this timestamp.
-
-.. .. c:type:: struct z_data_info_t
-
-..   Data structure containing meta informations about the associated data.
-
-..   .. c:member:: unsigned int flags
-
-..     Flags indicating which meta information is present in the :c:type:`z_data_info_t`:
-
-..       | ``Z_T_STAMP``
-..       | ``Z_KIND``
-..       | ``Z_ENCODING``
-
-..   .. c:member:: _z_timestamp_t tstamp
-
-..     The unique timestamp at which the data has been produced.
-
-..   .. c:member:: uint8_t encoding
-
-..     The encoding of the data.
-
-..   .. c:member:: unsigned short kind
-
-..     The kind of the data.
-
-.. .. c:type:: struct z_query_dest_t
-
-..   Data structure defining which storages or evals should be destination of a query (see :c:func:`z_query_wo`).
-
-..   .. c:member:: uint8_t kind
-
-..     One of the following destination kinds:
-
-..       | ``Z_BEST_MATCH`` the nearest complete storage/eval if there is one, all storages/evals if not.
-..       | ``Z_COMPLETE`` only complete storages/evals.
-..       | ``Z_ALL`` all storages/evals.
-..       | ``Z_NONE`` no storages/evals.
-
-..   .. c:member:: uint8_t nb
-
-..     The number of storages or evals that should be destination of the query when
-..     :c:member:`z_query_dest_t.kind` equals ``Z_COMPLETE``.
-
-.. .. c:type:: struct z_reply_value_t
-
-..   Data structure containing one of the replies to a query (see :c:type:`z_reply_handler_t`).
-
-..   .. c:member:: char kind
-
-..     One of the following kinds:
-
-..       | ``Z_STORAGE_DATA`` the reply contains some data from a storage.
-..       | ``Z_STORAGE_FINAL`` the reply indicates that no more data is expected from the specified storage.
-..       | ``Z_EVAL_DATA`` the reply contains some data from an eval.
-..       | ``Z_EVAL_FINAL`` the reply indicates that no more data is expected from the specified eval.
-..       | ``Z_REPLY_FINAL`` the reply indicates that no more replies are expected for the query.
-
-..   .. c:member:: const unsigned char *srcid
-
-..     The unique identifier of the storage or eval that sent the reply when :c:member:`z_reply_value_t.kind` equals
-..     ``Z_STORAGE_DATA``, ``Z_STORAGE_FINAL``, ``Z_EVAL_DATA`` or ``Z_EVAL_FINAL``.
-
-..   .. c:member:: size_t srcid_length
-
-..     The length of the :c:member:`z_reply_value_t.srcid` when :c:member:`z_reply_value_t.kind` equals
-..     ``Z_STORAGE_DATA``, ``Z_STORAGE_FINAL``, ``Z_EVAL_DATA`` or ``Z_EVAL_FINAL``.
-
-..   .. c:member:: _z_zint_t rsn
-
-..     The sequence number of the reply from the identified storage or eval when :c:member:`z_reply_value_t.kind` equals
-..     ``Z_STORAGE_DATA``, ``Z_STORAGE_FINAL``, ``Z_EVAL_DATA`` or ``Z_EVAL_FINAL``.
-
-..   .. c:member:: const char *rname
-
-..     The resource name of the received data when :c:member:`z_reply_value_t.kind` equals
-..     ``Z_STORAGE_DATA`` or ``Z_EVAL_DATA``.
-
-..   .. c:member:: const unsigned char *data
-
-..     A pointer to the received data when :c:member:`z_reply_value_t.kind` equals
-..     ``Z_STORAGE_DATA`` or ``Z_EVAL_DATA``.
-
-..   .. c:member:: size_t data_length
-
-..     The length of the received :c:member:`z_reply_value_t.data` when :c:member:`z_reply_value_t.kind` equals
-..     ``Z_STORAGE_DATA`` or ``Z_EVAL_DATA``.
-
-..   .. c:member:: z_data_info_t info
-
-..     Some meta information about the received :c:member:`z_reply_value_t.data` when :c:member:`z_reply_value_t.kind` equals
-..     ``Z_STORAGE_DATA`` or ``Z_EVAL_DATA``.
-
-.. .. c:type:: struct _z_property_t
-
-..   A key/value pair where the key is an integer and the value a byte sequence.
-
-..   .. c:member:: _z_zint_t id
-
-..     The key of the :c:type:`_z_property_t`.
-
-..   .. c:member:: z_array_uint8_t value
-
-..     The value of the :c:type:`_z_property_t`.
-
-Functions
----------
+Primitives
+~~~~~~~~~~
 
 .. autocfunction:: primitives.h::z_init_logger
 .. autocfunction:: primitives.h::z_keyexpr
@@ -499,46 +296,3 @@ Functions
 .. autocfunction:: primitives.h::zp_stop_lease_task
 .. autocfunction:: primitives.h::zp_read
 .. autocfunction:: primitives.h::zp_send_keep_alive
-
-.. Handlers
-.. --------
-
-.. .. c:type:: void (*_z_data_handler_t)(const z_resource_id_t *rid, const unsigned char *data, size_t length, const z_data_info_t *info, void *arg)
-
-..   Function to pass as argument of :c:func:`_z_declare_subscriber` or :c:func:`z_declare_storage`.
-..   It will be called on reception of data matching the subscribed/stored resource selection.
-
-..   | **rid** is the resource id of the received data.
-..   | **data** is a pointer to the received data.
-..   | **length** is the length of the received data.
-..   | **info** is the :c:type:`z_data_info_t` associated with the received data.
-..   | **arg** is the pointer passed to :c:func:`_z_declare_subscriber` or :c:func:`z_declare_storage`.
-
-.. .. c:type:: void (*_z_reply_handler_t)(const char *rname, const char *value_selector, z_replies_sender_t send_replies, void *query_handle, void *arg)
-
-..   Function to pass as argument of :c:func:`z_declare_storage` or :c:func:`z_declare_eval`.
-..   It will be called on reception of query matching the stored/evaluated resource selection.
-..   The :c:type:`_z_reply_handler_t` must provide the data matching the resource *rname* by calling
-..   the *send_replies* function with the *query_handle* and the data as arguments. The *send_replies*
-..   function MUST be called but accepts empty data array.
-
-..   | **rname** is the resource name of the queried data.
-..   | **value_selector** is a string provided by the querier refining the data to be provided.
-..   | **send_replies** is a function that MUST be called with the *query_handle* and the provided data as arguments.
-..   | **query_handle** is a pointer to pass as argument of *send_replies*.
-..   | **arg** is the pointer passed to :c:func:`z_declare_storage` or :c:func:`z_declare_eval`.
-
-.. .. c:type:: void (*z_reply_handler_t)(const z_reply_value_t *reply, void *arg)
-
-..   Function to pass as argument of :c:func:`z_query` or :c:func:`z_query_wo`.
-..   It will be called on reception of replies to the query sent by :c:func:`z_query` or :c:func:`z_query_wo`.
-
-..   | **reply** is the actual :c:type:`reply<z_reply_value_t>`.
-..   | **arg** is the pointer passed to :c:func:`z_query` or :c:func:`z_query_wo`.
-
-.. .. c:type:: void (*z_on_disconnect_t)(void *z)
-
-..   Function to pass as argument of :c:func:`_z_open`.
-..   It will be called each time the client API is disconnected from the infrastructure.
-
-..   | **z** is the zenoh-net session.
--- include/zenoh-pico/api/types.h
+++ include/zenoh-pico/api/types.h
@@ -20,63 +20,177 @@
 #include "zenoh-pico/net/query.h"
 #include "zenoh-pico/protocol/core.h"
 
-/* Loaned types */
+/**
+ * Represents a variable-length encoding unsigned integer.
+ *
+ * It is equivalent to the size of a ``size_t``.
+ */
 typedef _z_zint_t z_zint_t;
+
+/**
+ * Represents an array of bytes.
+ *
+ * Members:
+ *   size_t len: The length of the bytes array.
+ *   uint8_t *start: A pointer to the bytes array.
+ */
 typedef _z_bytes_t z_bytes_t;
 
-// Represents a Zenoh ID.
-// Valid Zenoh IDs are LSB-first 128bit unsigned and non-zero integers.
+/**
+ * Represents a Zenoh ID.
+ *
+ * In general, valid Zenoh IDs are LSB-first 128bit unsigned and non-zero integers.
+ *
+ * Members:
+ *   uint8_t id[16]: The array containing the 16 octets of a Zenoh ID.
+ */
 typedef struct
 {
     uint8_t id[16];
 } z_id_t;
 
+/**
+ * Represents a string without null-terminator.
+ *
+ * Members:
+ *   size_t len: The length of the string.
+ *   const char *val: A pointer to the string.
+ */
 typedef _z_string_t z_string_t;
+
+/**
+ * Represents a key expression in Zenoh.
+ *
+ * Operations over :c:type:`z_keyexpr_t` must be done using the provided functions:
+ *
+ *   - :c:func:`z_keyexpr`
+ *   - :c:func:`z_keyexpr_is_valid`
+ *   - :c:func:`z_keyexpr_to_string`
+ *   - :c:func:`zp_keyexpr_resolve`
+ */
 typedef _z_keyexpr_t z_keyexpr_t;
 
+/**
+ * Represents a Zenoh configuration.
+ *
+ * Configurations are usually used to set the parameters of a Zenoh session upon its opening.
+ *
+ * Operations over :c:type:`z_config_t` must be done using the provided functions:
+ *
+ *   - :c:func:`zp_config_new`
+ *   - :c:func:`zp_config_empty`
+ *   - :c:func:`zp_config_default`
+ *   - :c:func:`zp_keyexpr_resolve`
+ *   - :c:func:`zp_config_get`
+ *   - :c:func:`zp_config_insert`
+ */
 typedef _z_config_t z_config_t;
+
+/**
+ * Represents a Zenoh session.
+ */
 typedef _z_session_t z_session_t;
+
 typedef _z_config_t z_info_t;
+
+/**
+ * Represents a Zenoh (push) Subscriber entity.
+ *
+ * Operations over :c:type:`z_subscriber_t` must be done using the provided functions:
+ *
+ *   - :c:func:`z_declare_subscriber`
+ *   - :c:func:`z_undeclare_subscriber`
+ */
 typedef _z_subscriber_t z_subscriber_t;
+
+/**
+ * Represents a Zenoh Pull Subscriber entity.
+ *
+ * Operations over :c:type:`z_pull_subscriber_t` must be done using the provided functions:
+ *
+ *   - :c:func:`z_declare_pull_subscriber`
+ *   - :c:func:`z_undeclare_pull_subscriber`
+ *   - :c:func:`z_subscriber_pull`
+ */
 typedef _z_subscriber_t z_pull_subscriber_t;
+
+/**
+ * Represents a Zenoh Publisher entity.
+ *
+ * Operations over :c:type:`z_publisher_t` must be done using the provided functions:
+ *
+ *   - :c:func:`z_declare_publisher`
+ *   - :c:func:`z_undeclare_publisher`
+ *   - :c:func:`z_publisher_put`
+ *   - :c:func:`z_publisher_delete`
+ */
 typedef _z_publisher_t z_publisher_t;
+
+/**
+ * Represents a Zenoh Queryable entity.
+ *
+ * Operations over :c:type:`z_queryable_t` must be done using the provided functions:
+ *
+ *   - :c:func:`z_declare_queryable`
+ *   - :c:func:`z_undeclare_queryable`
+ */
 typedef _z_queryable_t z_queryable_t;
 
+/**
+ * Represents the encoding of a payload, in a MIME-like format.
+ *
+ * Members:
+ *   z_encoding_prefix_t prefix: The integer prefix of this encoding.
+ *   z_bytes_t suffix: The suffix of this encoding. It MUST be a valid UTF-8 string.
+ */
 typedef _z_encoding_t z_encoding_t;
+
 typedef _z_period_t z_period_t;
 
+/**
+ * Represents a Zenoh value.
+ *
+ * Members:
+ *   z_bytes_t payload: The payload of this zenoh value.
+ *   z_encoding_t encoding: The encoding of the `payload`.
+ */
 typedef struct
 {
     z_bytes_t payload;
     z_encoding_t encoding;
 } z_value_t;
 
-// /**
-//  * The kind of consolidation that should be applied on replies to a :c:func:`z_query`
-//  * at the different stages of the reply process.
-//  *
-//  * Members:
-//  *   first_routers: The consolidation mode to apply on first routers of the replies routing path.
-//  *   last_router: The consolidation mode to apply on last router of the replies routing path.
-//  *   reception: The consolidation mode to apply at reception of the replies.
-//  */
-// typedef struct
-// {
-//     z_consolidation_mode_t first_routers;
-//     z_consolidation_mode_t last_router;
-//     z_consolidation_mode_t reception;
-// } z_consolidation_strategy_t;
-
+/**
+ * Represents the set of options that can be applied to a (push) subscriber,
+ * upon its declaration via :c:func:`z_declare_subscriber`.
+ *
+ * Members:
+ *   z_reliability_t reliability: The subscription reliability.
+ */
 typedef struct
 {
   z_reliability_t reliability;
 } z_subscriber_options_t;
 
+/**
+ * Represents the set of options that can be applied to a pull subscriber,
+ * upon its declaration via :c:func:`z_declare_pull_subscriber`.
+ *
+ * Members:
+ *   z_reliability_t reliability: The subscription reliability.
+ */
 typedef struct
 {
     z_reliability_t reliability;
 } z_pull_subscriber_options_t;
 
+/**
+ * Represents the replies consolidation to apply on replies to a :c:func:`z_get`.
+ *
+ * Members:
+ *   z_query_consolidation_tag_t tag: Defines if the replies consolidation strategy is automatically or manually applied.
+ *   z_consolidation_strategy_t manual: The replies consolidation strategy to apply in case of `manual` configuration.
+ */
 typedef struct
 {
     z_query_consolidation_tag_t tag;
@@ -86,6 +200,15 @@ typedef struct
     };
 } z_query_consolidation_t;
 
+/**
+ * Represents the set of options that can be applied to a publisher,
+ * upon its declaration via :c:func:`z_declare_publisher`.
+ *
+ * Members:
+ *   int8_t local_routing: ``0``: disabled, ``1``: enabled, ``-1``: apply session level config.
+ *   z_congestion_control_t congestion_control: The congestion control to apply when routing messages from this publisher.
+ *   z_priority_t priority: The priority of messages issued by this publisher.
+ */
 typedef struct
 {
     int8_t local_routing;
@@ -93,46 +216,129 @@ typedef struct
     z_priority_t priority;
 } z_publisher_options_t;
 
+/**
+ * Represents the set of options that can be applied to a queryable,
+ * upon its declaration via :c:func:`z_declare_queryable`.
+ *
+ * Members:
+ *   uint8_t complete: The completeness of the queryable.
+ */
 typedef struct
 {
     uint8_t complete;
 } z_queryable_options_t;
 
+/**
+ * Represents the set of options that can be applied to a query reply,
+ * sent via :c:func:`z_query_reply`.
+ */
 typedef struct
 {
 } z_query_reply_options_t;
 
+/**
+ * Represents the set of options that can be applied to the put operation,
+ * whenever issued via :c:func:`z_put`.
+ *
+ * Members:
+ *   z_encoding_t encoding: The encoding of the payload.
+ *   z_congestion_control_t congestion_control: The congestion control to apply when routing this message.
+ *   uint8_t priority: The priority of this message.
+ */
 typedef struct
 {
     z_encoding_t encoding;
-    uint8_t congestion_control;
+    z_congestion_control_t congestion_control;
     uint8_t priority;
 } z_put_options_t;
 
+/**
+ * Represents the set of options that can be applied to the delete operation,
+ * whenever issued via :c:func:`z_delete`.
+ *
+ * Members:
+ *   z_congestion_control_t congestion_control: The congestion control to apply when routing this message.
+ */
 typedef struct
 {
     uint8_t congestion_control;
 } z_delete_options_t;
 
+/**
+ * Represents the set of options that can be applied to the put operation by a previously declared publisher,
+ * whenever issued via :c:func:`z_publisher_put`.
+ *
+ * Members:
+ *   z_encoding_t encoding: The encoding of the payload.
+ */
 typedef struct
 {
     z_encoding_t encoding;
 } z_publisher_put_options_t;
 
+/**
+ * Represents the set of options that can be applied to the delete operation by a previously declared publisher,
+ * whenever issued via :c:func:`z_publisher_delete`.
+ */
 typedef struct
 {
 } z_publisher_delete_options_t;
 
+/**
+ * Represents the set of options that can be applied to the get operation,
+ * whenever issued via :c:func:`z_get`.
+ *
+ * Members:
+ *   z_query_target_t target: The queryables that should be targeted by this get.
+ *   z_query_consolidation_t consolidation: The replies consolidation strategy to apply on replies.
+ */
 typedef struct
 {
     z_query_target_t target;
     z_query_consolidation_t consolidation;
 } z_get_options_t;
 
+/**
+ * Represents a data sample.
+ *
+ * A sample is the value associated to a given :c:type:`z_keyexpr_t` at a given point in time.
+ *
+ * Members:
+ *   z_keyexpr_t keyexpr: The keyexpr of this data sample.
+ *   z_bytes_t payload: The value of this data sample.
+ *   z_encoding_t encoding: The encoding of the value of this data sample.
+ *   z_sample_kind_t kind: The kind of this data sample (PUT or DELETE).
+ *   z_timestamp_t timestamp: The timestamp of this data sample.
+ */
 typedef _z_sample_t z_sample_t;
+
+/**
+ * Represents the content of a `hello` message returned by a zenoh entity as a reply to a `scout` message.
+ *
+ * Members:
+ *   unsigned int whatami: The kind of zenoh entity.
+ *   z_bytes_t pid: The Zenoh ID of the scouted entity (empty if absent).
+ *   z_str_array_t locators: The locators of the scouted entity.
+ */
 typedef _z_hello_t z_hello_t;
-typedef _z_reply_t z_reply_t;
+
+/**
+ * Represents the content of a reply to a query.
+ *
+ * Members:
+ *   z_sample_t sample: The :c:type:`_z_sample_t` containing the key and value of the reply.
+ *   unsigned int replier_kind: The kind of the replier that sent this reply.
+ *   z_bytes_t replier_id: The id of the replier that sent this reply.
+ */
 typedef _z_reply_data_t z_reply_data_t;
+
+/**
+ * Represents the reply to a query.
+ *
+ * Members:
+ *   z_reply_data_t data: the content of the reply.
+ */
+typedef _z_reply_t z_reply_t;
 
 #define _TYPEDEF_ARRAY(type, alias, elem, name)                               \
     typedef type alias;                                                       \
@@ -149,8 +355,37 @@ typedef _z_reply_data_t z_reply_data_t;
         return _z_##name##_array_is_empty(a);                                 \
     }                                                                         \
 
+/**
+ * Represents an array of ``char *``.
+ *
+ * Operations over :c:type:`z_str_array_t` must be done using the provided functions:
+ *
+ *   - ``char *z_str_array_get(z_str_array_t *a, size_t k);``
+ *   - ``size_t z_str_array_len(z_str_array_t *a);``
+ *   - ``uint8_t z_str_array_array_is_empty(z_str_array_t *a);``
+ */
 _TYPEDEF_ARRAY(_z_str_array_t, z_str_array_t, char *, str)
+
+/**
+ * Represents an array of ``z_hello_t``.
+ *
+ * Operations over :c:type:`z_hello_array_t` must be done using the provided functions:
+ *
+ *   - ``z_hello_t *z_hello_array_get(z_hello_array_t *a, size_t k);``
+ *   - ``size_t z_hello_array_len(z_hello_array_t *a);``
+ *   - ``uint8_t z_hello_array_array_is_empty(z_hello_array_t *a);``
+ */
 _TYPEDEF_ARRAY(_z_hello_array_t, z_hello_array_t, z_hello_t, hello)
+
+/**
+ * Represents an array of ``z_reply_data_t``.
+ *
+ * Operations over :c:type:`z_reply_data_array_t` must be done using the provided functions:
+ *
+ *   - ``z_reply_data_t *z_reply_data_array_get(z_reply_data_array_t *a, size_t k);``
+ *   - ``size_t z_reply_data_array_len(z_reply_data_array_t *a);``
+ *   - ``uint8_t z_reply_data_array_array_is_empty(z_reply_data_array_t *a);``
+ */
 _TYPEDEF_ARRAY(_z_reply_data_array_t, z_reply_data_array_t, z_reply_data_t, reply_data)
 
 /* Owned types */
@@ -190,6 +425,16 @@ _OWNED_TYPE(z_reply_data_array_t, reply_data_array)
 
 typedef void (*_z_dropper_handler_t)(void *arg);
 
+/**
+ * Represents the sample closure.
+ *
+ * A closure is a structure that contains all the elements for stateful, memory-leak-free callbacks.
+ *
+ * Members:
+ *   void *context: a pointer to an arbitrary state.
+ *   _z_data_handler_t call: `void *call(const struct z_sample_t*, const void *context)` is the callback function.
+ *   _z_dropper_handler_t drop: `void *drop(void*)` allows the callback's state to be freed.
+ */
 typedef struct
 {
   void *context;
@@ -197,6 +442,16 @@ typedef struct
   _z_dropper_handler_t drop;
 } z_owned_closure_sample_t;
 
+/**
+ * Represents the query callback closure.
+ *
+ * A closure is a structure that contains all the elements for stateful, memory-leak-free callbacks.
+ *
+ * Members:
+ *   void *context: a pointer to an arbitrary state.
+ *   _z_questionable_handler_t call: `void (*_z_questionable_handler_t)(z_query_t *query, void *arg)` is the callback function.
+ *   _z_dropper_handler_t drop: `void *drop(void*)` allows the callback's state to be freed.
+ */
 typedef struct
 {
   void *context;
@@ -205,6 +460,17 @@ typedef struct
 } z_owned_closure_query_t;
 
 typedef void (*z_owned_reply_handler_t)(z_owned_reply_t reply, void *arg);
+
+/**
+ * Represents the query reply callback closure.
+ *
+ * A closure is a structure that contains all the elements for stateful, memory-leak-free callbacks.
+ *
+ * Members:
+ *   void *context: a pointer to an arbitrary state.
+ *   z_owned_reply_handler_t call: `void (*z_owned_reply_handler_t)(z_owned_reply_t reply, void *arg)` is the callback function.
+ *   _z_dropper_handler_t drop: `void *drop(void*)` allows the callback's state to be freed.
+ */
 typedef struct
 {
   void *context;
@@ -213,6 +479,17 @@ typedef struct
 } z_owned_closure_reply_t;
 
 typedef void (*z_id_handler_t)(const z_id_t *id, void *arg);
+
+/**
+ * Represents the Zenoh ID callback closure.
+ *
+ * A closure is a structure that contains all the elements for stateful, memory-leak-free callbacks.
+ *
+ * Members:
+ *   void *context: a pointer to an arbitrary state.
+ *   z_id_handler_t call: `void (*z_id_handler_t)(const z_id_t *id, void *arg)` is the callback function.
+ *   _z_dropper_handler_t drop: `void *drop(void*)` allows the callback's state to be freed.
+ */
 typedef struct
 {
   void *context;
