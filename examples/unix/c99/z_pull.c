efa90ac445a40bb61c8a3faacc2ce47140890843
Carlos Guimarães <carlos.em.guimaraes@gmail.com>
2023-02-06 11:24:28 +0100

    Fix C99 examples failed build introduced in #150

--- examples/unix/c99/z_pull.c
+++ examples/unix/c99/z_pull.c
@@ -21,9 +21,9 @@
 void data_handler(const z_sample_t *sample, void *ctx) {
     (void)(ctx);
     z_owned_str_t keystr = z_keyexpr_to_string(sample->keyexpr);
-    printf(">> [Subscriber] Received ('%s': '%.*s')\n", z_loan(keystr), (int)sample->payload.len,
+    printf(">> [Subscriber] Received ('%s': '%.*s')\n", z_str_loan(&keystr), (int)sample->payload.len,
            sample->payload.start);
-    z_drop(z_move(keystr));
+    z_str_drop(z_str_move(&keystr));
 }
 
 int main(int argc, char **argv) {

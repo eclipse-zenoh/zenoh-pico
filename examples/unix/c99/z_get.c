eclipse-zenoh/zenoh-pico

commit c9c8752e6e5fd6a0f185700982ffe01e8103f4e9

User have to deallocate memory allocated by zenoh-pico (#145) (#150)
* Rename _z_str_t to _z_str_se_t

* Make char* allocation made by Zenoh explicit

* Expand owned types to avoid workarounds and extra allocations

* String is a special case for owned types

* Fix examples that are not C11

* Have both z_str_copy and z_str_n_copy to avoid parsing the string the twice

examples/unix/c99/z_get.c
@@ -27,9 +27,9 @@ void reply_handler(z_owned_reply_t *reply, void *ctx) {
     (void)(ctx);
     if (z_reply_is_ok(reply)) {
         z_sample_t sample = z_reply_ok(reply);
-        char *keystr = z_keyexpr_to_string(sample.keyexpr);
-        printf(">> Received ('%s': '%.*s')\n", keystr, (int)sample.payload.len, sample.payload.start);
-        free(keystr);
+        z_owned_str_t keystr = z_keyexpr_to_string(sample.keyexpr);
+        printf(">> Received ('%s': '%.*s')\n", z_loan(keystr), (int)sample.payload.len, sample.payload.start);
+        z_drop(z_move(keystr));
     } else {
         printf(">> Received an error\n");
     }

examples/unix/c99/z_pull.c
@@ -20,9 +20,10 @@
 
 void data_handler(const z_sample_t *sample, void *ctx) {
     (void)(ctx);
-    char *keystr = z_keyexpr_to_string(sample->keyexpr);
-    printf(">> [Subscriber] Received ('%s': '%.*s')\n", keystr, (int)sample->payload.len, sample->payload.start);
-    free(keystr);
+    z_owned_str_t keystr = z_keyexpr_to_string(sample->keyexpr);
+    printf(">> [Subscriber] Received ('%s': '%.*s')\n", z_loan(keystr), (int)sample->payload.len,
+           sample->payload.start);
+    z_drop(z_move(keystr));
 }
 
 int main(int argc, char **argv) {

examples/unix/c99/z_queryable.c
@@ -23,13 +23,13 @@ const char *value = "Queryable from Pico!";
 
 void query_handler(const z_query_t *query, void *ctx) {
     (void)(ctx);
-    char *keystr = z_keyexpr_to_string(z_query_keyexpr(query));
+    z_owned_str_t keystr = z_keyexpr_to_string(z_query_keyexpr(query));
     z_bytes_t pred = z_query_parameters(query);
-    printf(" >> [Queryable handler] Received Query '%s%.*s'\n", keystr, (int)pred.len, pred.start);
+    printf(" >> [Queryable handler] Received Query '%s%.*s'\n", z_loan(keystr), (int)pred.len, pred.start);
     z_query_reply_options_t options = z_query_reply_options_default();
     options.encoding = z_encoding(Z_ENCODING_PREFIX_TEXT_PLAIN, NULL);
     z_query_reply(query, z_keyexpr(keyexpr), (const unsigned char *)value, strlen(value), &options);
-    free(keystr);
+    z_drop(z_move(keystr));
 }
 
 int main(int argc, char **argv) {

examples/unix/c99/z_sub.c
@@ -21,9 +21,10 @@
 
 void data_handler(const z_sample_t *sample, void *arg) {
     (void)(arg);
-    char *keystr = z_keyexpr_to_string(sample->keyexpr);
-    printf(">> [Subscriber] Received ('%s': '%.*s')\n", keystr, (int)sample->payload.len, sample->payload.start);
-    free(keystr);
+    z_owned_str_t keystr = z_keyexpr_to_string(sample->keyexpr);
+    printf(">> [Subscriber] Received ('%s': '%.*s')\n", z_loan(keystr), (int)sample->payload.len,
+           sample->payload.start);
+    z_drop(z_move(keystr));
 }
 
 int main(int argc, char **argv) {

examples/unix/c99/z_sub_st.c
@@ -21,9 +21,10 @@
 
 void data_handler(const z_sample_t *sample, void *arg) {
     (void)(arg);
-    char *keystr = z_keyexpr_to_string(sample->keyexpr);
-    printf(">> [Subscriber] Received ('%s': '%.*s')\n", keystr, (int)sample->payload.len, sample->payload.start);
-    free(keystr);
+    z_owned_str_t keystr = z_keyexpr_to_string(sample->keyexpr);
+    printf(">> [Subscriber] Received ('%s': '%.*s')\n", z_loan(keystr), (int)sample->payload.len,
+           sample->payload.start);
+    z_drop(z_move(keystr));
 }
 
 int main(int argc, char **argv) {

examples/espidf/z_get.c
@@ -105,9 +105,9 @@ void reply_dropper(void *ctx) { printf(" >> Received query final notification\n"
 void reply_handler(z_owned_reply_t *oreply, void *ctx) {
     if (z_reply_is_ok(oreply)) {
         z_sample_t sample = z_reply_ok(oreply);
-        char *keystr = z_keyexpr_to_string(sample.keyexpr);
-        printf(" >> Received ('%s': '%.*s')\n", keystr, (int)sample.payload.len, sample.payload.start);
-        free(keystr);
+        z_owned_str_t keystr = z_keyexpr_to_string(sample.keyexpr);
+        printf(" >> Received ('%s': '%.*s')\n", z_loan(keystr), (int)sample.payload.len, sample.payload.start);
+        z_drop(z_move(keystr));
     } else {
         printf(" >> Received an error\n");
     }

examples/espidf/z_pull.c
@@ -100,10 +100,10 @@ void wifi_init_sta(void) {
 }
 
 void data_handler(const z_sample_t* sample, void* arg) {
-    char *keystr = z_keyexpr_to_string(sample->keyexpr);
-    printf(" >> [Subscriber handler] Received ('%s': '%.*s')\n", keystr,
-           (int)sample->payload.len, sample->payload.start);
-    free(keystr);
+    z_owned_str_t keystr = z_keyexpr_to_string(sample->keyexpr);
+    printf(" >> [Subscriber handler] Received ('%s': '%.*s')\n", z_loan(keystr), (int)sample->payload.len,
+           sample->payload.start);
+    z_drop(z_move(keystr));
 }
 
 void app_main() {

examples/espidf/z_queryable.c
@@ -102,11 +102,11 @@ void wifi_init_sta(void) {
 
 void query_handler(z_query_t *query, void *ctx) {
     (void)(ctx);
-    char *keystr = z_keyexpr_to_string(z_query_keyexpr(query));
+    z_owned_str_t keystr = z_keyexpr_to_string(z_query_keyexpr(query));
     z_bytes_t pred = z_query_parameters(query);
-    printf(">> [Queryable handler] Received Query '%s%.*s'\n", keystr, (int)pred.len, pred.start);
+    printf(">> [Queryable handler] Received Query '%s%.*s'\n", z_loan(keystr), (int)pred.len, pred.start);
     z_query_reply(query, z_keyexpr(KEYEXPR), (const unsigned char *)VALUE, strlen(VALUE), NULL);
-    free(keystr);
+    z_drop(z_move(keystr));
 }
 
 void app_main() {

commit efa90ac445a40bb61c8a3faacc2ce47140890843

Fix C99 examples failed build introduced in #150

examples/unix/c99/z_get.c
@@ -28,8 +28,8 @@ void reply_handler(z_owned_reply_t *reply, void *ctx) {
     if (z_reply_is_ok(reply)) {
         z_sample_t sample = z_reply_ok(reply);
         z_owned_str_t keystr = z_keyexpr_to_string(sample.keyexpr);
-        printf(">> Received ('%s': '%.*s')\n", z_loan(keystr), (int)sample.payload.len, sample.payload.start);
-        z_drop(z_move(keystr));
+        printf(">> Received ('%s': '%.*s')\n", z_str_loan(&keystr), (int)sample.payload.len, sample.payload.start);
+        z_str_drop(z_str_move(&keystr));
     } else {
         printf(">> Received an error\n");
     }

examples/unix/c99/z_pull.c
@@ -21,9 +21,9 @@
 void data_handler(const z_sample_t *sample, void *ctx) {
     (void)(ctx);
     z_owned_str_t keystr = z_keyexpr_to_string(sample->keyexpr);
-    printf(">> [Subscriber] Received ('%s': '%.*s')\n", z_loan(keystr), (int)sample->payload.len,
+    printf(">> [Subscriber] Received ('%s': '%.*s')\n", z_str_loan(&keystr), (int)sample->payload.len,
            sample->payload.start);
-    z_drop(z_move(keystr));
+    z_str_drop(z_str_move(&keystr));
 }
 
 int main(int argc, char **argv) {

examples/unix/c99/z_queryable.c
@@ -25,11 +25,11 @@ void query_handler(const z_query_t *query, void *ctx) {
     (void)(ctx);
     z_owned_str_t keystr = z_keyexpr_to_string(z_query_keyexpr(query));
     z_bytes_t pred = z_query_parameters(query);
-    printf(" >> [Queryable handler] Received Query '%s%.*s'\n", z_loan(keystr), (int)pred.len, pred.start);
+    printf(" >> [Queryable handler] Received Query '%s%.*s'\n", z_str_loan(&keystr), (int)pred.len, pred.start);
     z_query_reply_options_t options = z_query_reply_options_default();
     options.encoding = z_encoding(Z_ENCODING_PREFIX_TEXT_PLAIN, NULL);
     z_query_reply(query, z_keyexpr(keyexpr), (const unsigned char *)value, strlen(value), &options);
-    z_drop(z_move(keystr));
+    z_str_drop(z_str_move(&keystr));
 }
 
 int main(int argc, char **argv) {

examples/unix/c99/z_sub.c
@@ -22,9 +22,9 @@
 void data_handler(const z_sample_t *sample, void *arg) {
     (void)(arg);
     z_owned_str_t keystr = z_keyexpr_to_string(sample->keyexpr);
-    printf(">> [Subscriber] Received ('%s': '%.*s')\n", z_loan(keystr), (int)sample->payload.len,
+    printf(">> [Subscriber] Received ('%s': '%.*s')\n", z_str_loan(&keystr), (int)sample->payload.len,
            sample->payload.start);
-    z_drop(z_move(keystr));
+    z_str_drop(z_str_move(&keystr));
 }
 
 int main(int argc, char **argv) {

examples/unix/c99/z_sub_st.c
@@ -22,9 +22,9 @@
 void data_handler(const z_sample_t *sample, void *arg) {
     (void)(arg);
     z_owned_str_t keystr = z_keyexpr_to_string(sample->keyexpr);
-    printf(">> [Subscriber] Received ('%s': '%.*s')\n", z_loan(keystr), (int)sample->payload.len,
+    printf(">> [Subscriber] Received ('%s': '%.*s')\n", z_str_loan(&keystr), (int)sample->payload.len,
            sample->payload.start);
-    z_drop(z_move(keystr));
+    z_str_drop(z_str_move(&keystr));
 }
 
 int main(int argc, char **argv) {