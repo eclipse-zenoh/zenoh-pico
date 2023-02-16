adb4f0f8b0af7a927d1191689174159db8617518
Carlos Guimarães <carlos.em.guimaraes@gmail.com>
2023-02-16 12:19:07 +0100

    Add UUID utils

---
+++ include/zenoh-pico/utils/uuid.h
@@ -0,0 +1,37 @@
+//
+// Copyright (c) 2022 ZettaScale Technology
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+//
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/**
+ * Converts an UUID in string format to a byte array.
+ *
+ * Parameters:
+ *   bytes: Pointer to an already allocated byte array of size (at least) 16 bytes.
+ *   uuid_str: A valid UUID string.
+ */
+void _z_uuid_to_bytes(uint8_t *bytes, const char *uuid_str);
+
+/**
+ * Converts a byte array representing an UUID to its string representation.
+ *
+ * Parameters:
+ *   bytes: Pointer to the byte array containing the UUID.
+ *
+ * Returns:
+ *   The `char *` of the UUID in string format.
+ */
+char *_z_bytes_to_uuid(const uint8_t *uuid);
