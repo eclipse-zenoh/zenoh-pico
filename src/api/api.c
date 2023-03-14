baa27fee29c58245404471855a2ffd26d5bf5dd2
Michael Ilyin <milyin@gmail.com>
2023-03-14 11:54:46 +0000

    create null owned keyexpr if null parameter passed

--- src/api/api.c
+++ src/api/api.c
@@ -671,7 +671,7 @@ int8_t z_get(z_session_t zs, z_keyexpr_t keyexpr, const char *parameters, z_owne
 z_owned_keyexpr_t z_keyexpr_new(const char *name) {
     z_owned_keyexpr_t key;
 
-    key._value = (z_keyexpr_t *)z_malloc(sizeof(z_keyexpr_t));
+    key._value = name ? (z_keyexpr_t *)z_malloc(sizeof(z_keyexpr_t)) : NULL;
     if (key._value != NULL) {
         *key._value = _z_rid_with_suffix(Z_RESOURCE_ID_NONE, name);
     }
