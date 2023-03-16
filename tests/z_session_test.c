a0320ed704ecad8816a1b5dd7b9044d466d752c3
Michael Ilyin <milyin@gmail.com>
2023-03-16 16:44:43 +0000

    session test

---
+++ tests/z_session_test.c
@@ -0,0 +1,30 @@
+//
+// Copyright (c) 2022 ZettaScale Technology
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "zenoh-pico.h"
+
+#undef NDEBUG
+#include <assert.h>
+
+int main(void) {
+    z_owned_config_t config = z_config_default();
+    zp_config_insert(z_loan(config), Z_CONFIG_MODE_KEY, z_string_make("client"));
+    z_owned_session_t s = z_open(z_move(config));
+    assert(z_check(s));
+    z_keyexpr_t ke = z_keyexpr("FOO/BAR");
+}
