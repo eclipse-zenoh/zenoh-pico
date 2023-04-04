718ba2b9affb6c1b8124e9fd90d56044e0cd9837
Carlos Guimarães <carlos.em.guimaraes@gmail.com>
2023-04-04 16:56:45 +0200

    Update return value type

--- src/transport/unicast/link/task/read.c
+++ src/transport/unicast/link/task/read.c
@@ -40,8 +40,6 @@ void *_zp_unicast_read_task(void *ztu_arg) {
     _z_transport_unicast_t *ztu = (_z_transport_unicast_t *)ztu_arg;
     ztu->_read_task_running = true;
 
-    uint8_t ret;
-
     // Acquire and keep the lock
     _z_mutex_lock(&ztu->_mutex_rx);
 
@@ -88,7 +86,7 @@ void *_zp_unicast_read_task(void *ztu_arg) {
 
         // Decode one session message
         _z_transport_message_t t_msg;
-        ret = _z_transport_message_decode_na(&t_msg, &zbuf);
+        int8_t ret = _z_transport_message_decode_na(&t_msg, &zbuf);
 
         if (ret == _Z_RES_OK) {
             ret = _z_unicast_handle_transport_message(ztu, &t_msg);
