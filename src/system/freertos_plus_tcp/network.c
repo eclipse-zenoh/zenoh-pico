f949eb374b34713e7e01dda172392b7e54c4c043
Błażej Sowa <blazej@fictionlab.pl>
2023-08-14 18:51:35 +0200

    Add TCP transport

--- src/system/freertos_plus_tcp/network.c
+++ src/system/freertos_plus_tcp/network.c
@@ -8,6 +8,94 @@
 #include "FreeRTOS.h"
 #include "FreeRTOS_IP.h"
 
+#if Z_LINK_TCP == 1
+/*------------------ TCP sockets ------------------*/
+int8_t _z_create_endpoint_tcp(_z_sys_net_endpoint_t *ep, const char *s_address, const char *s_port) {
+    int8_t ret = _Z_RES_OK;
+
+    if (FreeRTOS_getaddrinfo(s_address, NULL, NULL, &ep->_iptcp) < 0) {
+        ret = _Z_ERR_GENERIC;
+    }
+
+    ep->_iptcp->ai_addr->sin_family = FREERTOS_AF_INET4;
+
+    // Parse and check the validity of the port
+    uint32_t port = strtoul(s_port, NULL, 10);
+    if ((port > (uint32_t)0) && (port <= (uint32_t)65355)) {  // Port numbers should range from 1 to 65355
+        ep->_iptcp->ai_addr->sin_port = FreeRTOS_htons((uint16_t)port);
+    } else {
+        ret = _Z_ERR_GENERIC;
+    }
+
+    return ret;
+}
+
+void _z_free_endpoint_tcp(_z_sys_net_endpoint_t *ep) { FreeRTOS_freeaddrinfo(ep->_iptcp); }
+
+int8_t _z_open_tcp(_z_sys_net_socket_t *sock, const _z_sys_net_endpoint_t rep, uint32_t tout) {
+    int8_t ret = _Z_RES_OK;
+
+    sock->_socket = FreeRTOS_socket(rep._iptcp->ai_family, FREERTOS_SOCK_STREAM, FREERTOS_IPPROTO_TCP);
+    if (sock->_socket != FREERTOS_INVALID_SOCKET) {
+        TickType_t receive_timeout = pdMS_TO_TICKS(tout);
+
+        if (FreeRTOS_setsockopt(sock->_socket, 0, FREERTOS_SO_RCVTIMEO, &receive_timeout, 0) != 0) {
+            ret = _Z_ERR_GENERIC;
+        } else if (FreeRTOS_connect(sock->_socket, rep._iptcp->ai_addr, rep._iptcp->ai_addrlen) < 0) {
+            ret = _Z_ERR_GENERIC;
+        }
+    } else {
+        ret = _Z_ERR_GENERIC;
+    }
+
+    return ret;
+}
+
+int8_t _z_listen_tcp(_z_sys_net_socket_t *sock, const _z_sys_net_endpoint_t lep) {
+    int8_t ret = _Z_RES_OK;
+    (void)sock;
+    (void)lep;
+
+    // @TODO: To be implemented
+    ret = _Z_ERR_GENERIC;
+
+    return ret;
+}
+
+void _z_close_tcp(_z_sys_net_socket_t *sock) { FreeRTOS_closesocket(sock->_socket); }
+
+size_t _z_read_tcp(const _z_sys_net_socket_t sock, uint8_t *ptr, size_t len) {
+    ssize_t rb = FreeRTOS_recv(sock._socket, ptr, len, 0);
+    if (rb < (ssize_t)0) {
+        rb = SIZE_MAX;
+    }
+
+    return rb;
+}
+
+size_t _z_read_exact_tcp(const _z_sys_net_socket_t sock, uint8_t *ptr, size_t len) {
+    size_t n = 0;
+    uint8_t *pos = &ptr[0];
+
+    do {
+        size_t rb = _z_read_tcp(sock, pos, len - n);
+        if (rb == SIZE_MAX) {
+            n = rb;
+            break;
+        }
+
+        n = n + rb;
+        pos = _z_ptr_u8_offset(pos, n);
+    } while (n != len);
+
+    return n;
+}
+
+size_t _z_send_tcp(const _z_sys_net_socket_t sock, const uint8_t *ptr, size_t len) {
+    return FreeRTOS_send(sock._socket, ptr, len, 0);
+}
+#endif
+
 #if Z_LINK_UDP_UNICAST == 1 || Z_LINK_UDP_MULTICAST == 1
 /*------------------ UDP sockets ------------------*/
 int8_t _z_create_endpoint_udp(_z_sys_net_endpoint_t *ep, const char *s_address, const char *s_port) {
