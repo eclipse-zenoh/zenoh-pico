fe5d30e26bdfb904d4f6103a740906cb2d0fa43e
Błażej Sowa <blazej@fictionlab.pl>
2023-08-28 15:59:46 +0200

    Add more examples
    Signed-off-by: Błażej Sowa <blazej@fictionlab.pl>

---
+++ examples/freertos_plus_tcp/z_sub_st.c
@@ -0,0 +1,69 @@
+//
+// Copyright (c) 2022 ZettaScale Technology
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+//   Błażej Sowa, <blazej@fictionlab.pl>
+
+#include <zenoh-pico.h>
+
+#define CLIENT_OR_PEER 0  // 0: Client mode; 1: Peer mode
+#if CLIENT_OR_PEER == 0
+#define MODE "client"
+#define PEER ""  // If empty, it will scout
+#elif CLIENT_OR_PEER == 1
+#define MODE "peer"
+#define PEER "udp/224.0.0.225:7447#iface=en0"
+#else
+#error "Unknown Zenoh operation mode. Check CLIENT_OR_PEER value."
+#endif
+
+#define KEYEXPR "demo/example/**"
+
+void data_handler(const z_sample_t *sample, void *ctx) {
+    (void)(ctx);
+    z_owned_str_t keystr = z_keyexpr_to_string(sample->keyexpr);
+    printf(">> [Subscriber] Received ('%s': '%.*s')\n", z_loan(keystr), (int)sample->payload.len,
+           sample->payload.start);
+    z_drop(z_move(keystr));
+}
+
+void app_main() {
+    z_owned_config_t config = z_config_default();
+    zp_config_insert(z_loan(config), Z_CONFIG_MODE_KEY, z_string_make(MODE));
+    if (strcmp(PEER, "") != 0) {
+        zp_config_insert(z_loan(config), Z_CONFIG_PEER_KEY, z_string_make(PEER));
+    }
+
+    printf("Opening session...\n");
+    z_owned_session_t s = z_open(z_move(config));
+    if (!z_check(s)) {
+        printf("Unable to open session!\n");
+        return;
+    }
+
+    z_owned_closure_sample_t callback = z_closure(data_handler);
+    printf("Declaring Subscriber on '%s'...\n", KEYEXPR);
+    z_owned_subscriber_t sub = z_declare_subscriber(z_loan(s), z_keyexpr(KEYEXPR), z_move(callback), NULL);
+    if (!z_check(sub)) {
+        printf("Unable to declare subscriber.\n");
+        return;
+    }
+
+    while (1) {
+        zp_read(z_loan(s), NULL);
+        zp_send_keep_alive(z_loan(s), NULL);
+        zp_send_join(z_loan(s), NULL);
+    }
+
+    z_undeclare_subscriber(z_move(sub));
+
+    z_close(z_move(s));
+}
