3cf9ac58df6c5e43fb3648fb77a142931abd64be
Jean-Roland <jean.roland.gosse@gmail.com>
2023-11-09 16:54:29 +0100

    feat: displace functions in transport files

--- src/transport/manager.c
+++ src/transport/manager.c
@@ -22,79 +22,62 @@
 
 int8_t _z_new_transport_client(_z_transport_t *zt, char *locator, _z_id_t *local_zid) {
     int8_t ret = _Z_RES_OK;
-
+    // Init link
     _z_link_t zl;
     memset(&zl, 0, sizeof(_z_link_t));
-
+    // Open link
     ret = _z_open_link(&zl, locator);
-    if (ret == _Z_RES_OK) {
-#if Z_FEATURE_UNICAST_TRANSPORT == 1
-        if (_Z_LINK_IS_MULTICAST(zl._capabilities) == false) {
-            _z_transport_unicast_establish_param_t tp_param;
-            ret = _z_transport_unicast_open_client(&tp_param, &zl, local_zid);
-            if (ret == _Z_RES_OK) {
-                ret = _z_transport_unicast(zt, &zl, &tp_param);
-            } else {
-                _z_link_clear(&zl);
-            }
-        } else
-#endif  // Z_FEATURE_UNICAST_TRANSPORT == 1
-#if Z_FEATURE_MULTICAST_TRANSPORT == 1
-            if (_Z_LINK_IS_MULTICAST(zl._capabilities) == true) {
-            _z_transport_multicast_establish_param_t tp_param;
-            ret = _z_transport_multicast_open_client(&tp_param, &zl, local_zid);
-            if (ret == _Z_RES_OK) {
-                ret = _z_transport_multicast(zt, &zl, &tp_param);
-            } else {
-                _z_link_clear(&zl);
-            }
-        } else
-#endif  // Z_FEATURE_MULTICAST_TRANSPORT == 1
-        {
-            _z_link_clear(&zl);
-            ret = _Z_ERR_TRANSPORT_NOT_AVAILABLE;
-        }
+    if (ret != _Z_RES_OK) {
+        return ret;
     }
-
+    // Open transport
+    if (_Z_LINK_IS_MULTICAST(zl._capabilities)) {
+        _z_transport_multicast_establish_param_t tp_param;
+        ret = _z_multicast_open_client(&tp_param, &zl, local_zid);
+        if (ret != _Z_RES_OK) {
+            _z_link_clear(&zl);
+            return ret;
+        }
+        ret = _z_multicast_transport_create(zt, &zl, &tp_param);
+    } else {
+        _z_transport_unicast_establish_param_t tp_param;
+        ret = _z_unicast_open_client(&tp_param, &zl, local_zid);
+        if (ret != _Z_RES_OK) {
+            _z_link_clear(&zl);
+            return ret;
+        }
+        ret = _z_unicast_transport_create(zt, &zl, &tp_param);
+    }
     return ret;
 }
 
 int8_t _z_new_transport_peer(_z_transport_t *zt, char *locator, _z_id_t *local_zid) {
     int8_t ret = _Z_RES_OK;
-
+    // Init link
     _z_link_t zl;
     memset(&zl, 0, sizeof(_z_link_t));
-
+    // Listen link
     ret = _z_listen_link(&zl, locator);
-    if (ret == _Z_RES_OK) {
-#if Z_FEATURE_UNICAST_TRANSPORT == 1
-        if (_Z_LINK_IS_MULTICAST(zl._capabilities) == false) {
-            _z_transport_unicast_establish_param_t tp_param;
-            ret = _z_transport_unicast_open_peer(&tp_param, &zl, local_zid);
-            if (ret == _Z_RES_OK) {
-                ret = _z_transport_unicast(zt, &zl, &tp_param);
-            } else {
-                _z_link_clear(&zl);
-            }
-        } else
-#endif  // Z_FEATURE_UNICAST_TRANSPORT == 1
-#if Z_FEATURE_MULTICAST_TRANSPORT == 1
-            if (_Z_LINK_IS_MULTICAST(zl._capabilities) == true) {
-            _z_transport_multicast_establish_param_t tp_param;
-            ret = _z_transport_multicast_open_peer(&tp_param, &zl, local_zid);
-            if (ret == _Z_RES_OK) {
-                ret = _z_transport_multicast(zt, &zl, &tp_param);
-            } else {
-                _z_link_clear(&zl);
-            }
-        } else
-#endif  // Z_FEATURE_MULTICAST_TRANSPORT == 1
-        {
-            _z_link_clear(&zl);
-            ret = _Z_ERR_TRANSPORT_NOT_AVAILABLE;
-        }
+    if (ret != _Z_RES_OK) {
+        return ret;
     }
-
+    if (_Z_LINK_IS_MULTICAST(zl._capabilities)) {
+        _z_transport_multicast_establish_param_t tp_param;
+        ret = _z_multicast_open_peer(&tp_param, &zl, local_zid);
+        if (ret != _Z_RES_OK) {
+            _z_link_clear(&zl);
+            return ret;
+        }
+        ret = _z_multicast_transport_create(zt, &zl, &tp_param);
+    } else {
+        _z_transport_unicast_establish_param_t tp_param;
+        ret = _z_unicast_open_peer(&tp_param, &zl, local_zid);
+        if (ret != _Z_RES_OK) {
+            _z_link_clear(&zl);
+            return ret;
+        }
+        ret = _z_unicast_transport_create(zt, &zl, &tp_param);
+    }
     return ret;
 }
 
