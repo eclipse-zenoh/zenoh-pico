9cf5c97ea6ee2601a42cb33e32c1dbf701aaf8f1
Jean-Roland <jean.roland.gosse@gmail.com>
2023-11-24 16:04:49 +0100

    feat: add raweth config files

---
+++ include/zenoh-pico/transport/raweth/config.h
@@ -0,0 +1,41 @@
+//
+// Copyright (c) 2022 ZettaScale Technology
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+//
+
+#ifndef ZENOH_PICO_RAWETH_CONFIG_H
+#define ZENOH_PICO_RAWETH_CONFIG_H
+
+#include <stdbool.h>
+
+#include "zenoh-pico/system/link/raweth.h"
+#include "zenoh-pico/transport/transport.h"
+#include "zenoh-pico/utils/result.h"
+#include "zenoh-pico/protocol/core.h"
+
+#if Z_FEATURE_RAWETH_TRANSPORT == 1
+
+typedef struct {
+    _z_keyexpr_t _keyexpr;
+    uint16_t _vlan;  // vlan tag (pcp + dei + id)
+    uint8_t _dmac[_ZP_MAC_ADDR_LENGTH];
+    _Bool _has_vlan;
+} _zp_raweth_cfg_entry;
+
+// Source mac address
+extern const uint8_t _zp_raweth_cfg_smac[_ZP_MAC_ADDR_LENGTH];
+
+// Sort the keyexpr alphabetically to use binary search (if size ~100+), otherwise use simple linear search
+extern const _zp_raweth_cfg_entry _zp_raweth_cfg_array[];
+
+#endif  // Z_FEATURE_RAWETH_TRANSPORT == 1
+#endif  // ZENOH_PICO_RAWETH_CONFIG_H
---
+++ src/transport/raweth/config.c
@@ -0,0 +1,29 @@
+//
+// Copyright (c) 2022 ZettaScale Technology
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+//
+
+#include "zenoh-pico/transport/raweth/config.h"
+
+#if Z_FEATURE_RAWETH_TRANSPORT == 1
+
+// Should be generated
+const uint8_t _zp_raweth_cfg_smac[_ZP_MAC_ADDR_LENGTH] = {0x30, 0x03, 0xc8, 0x37, 0x25, 0xa1};
+
+// Should be generated
+const _zp_raweth_cfg_entry _zp_raweth_cfg_array[] = {
+    {{0, {0}, ""}, 0x00, {0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff}, false}, // Default mac addr
+    {{0, {0},"some/key/expr"}, 0x8c, {0x00, 0x11, 0x22, 0x33, 0x44, 0x55}, true}, // entry1
+    {{0, {0},"another/keyexpr"}, 0x43, {0x01, 0x23, 0x45, 0x67, 0x89, 0xab}, true}, // entry2
+};
+
+#endif  // Z_FEATURE_RAWETH_TRANSPORT == 1
