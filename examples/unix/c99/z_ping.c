170a12a9880303ba06a788e15b668857306a72cd
Jean-Roland <jean.roland.gosse@gmail.com>
2024-01-18 17:06:13 +0100

    fix: missing newline on task start fail log

--- examples/unix/c99/z_ping.c
+++ examples/unix/c99/z_ping.c
@@ -73,7 +73,7 @@ int main(int argc, char** argv) {
 
     if (zp_start_read_task(z_session_loan(&session), NULL) < 0 ||
         zp_start_lease_task(z_session_loan(&session), NULL) < 0) {
-        printf("Unable to start read and lease tasks");
+        printf("Unable to start read and lease tasks\n");
         return -1;
     }
 
