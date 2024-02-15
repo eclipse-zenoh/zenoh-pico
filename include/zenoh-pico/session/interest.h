6c6abf9d3f6327f18024fbf2d1568f8689ae99c9
Jean-Roland <jean.roland.gosse@gmail.com>
2024-02-15 10:40:26 +0100

    fix: exclude dummy functions from config token

--- include/zenoh-pico/session/interest.h
+++ include/zenoh-pico/session/interest.h
@@ -30,15 +30,15 @@
 
 _z_session_interest_rc_t *_z_get_interest_by_id(_z_session_t *zn, const _z_zint_t id);
 _z_session_interest_rc_list_t *_z_get_interest_by_key(_z_session_t *zn, const _z_keyexpr_t key);
-
 _z_session_interest_rc_t *_z_register_interest(_z_session_t *zn, _z_session_interest_t *intr);
-int8_t _z_interest_process_declare(_z_session_t *zn, const _z_declaration_t *decl);
-int8_t _z_interest_process_undeclare(_z_session_t *zn, const _z_declaration_t *decl);
 void _z_unregister_interest(_z_session_t *zn, _z_session_interest_rc_t *intr);
 void _z_flush_interest(_z_session_t *zn);
+#endif  // Z_FEATURE_INTEREST == 1
+
+int8_t _z_interest_process_declare(_z_session_t *zn, const _z_declaration_t *decl);
+int8_t _z_interest_process_undeclare(_z_session_t *zn, const _z_declaration_t *decl);
 int8_t _z_process_final_interest(_z_session_t *zn, uint32_t id);
 int8_t _z_process_undeclare_interest(_z_session_t *zn, uint32_t id);
 int8_t _z_process_declare_interest(_z_session_t *zn, _z_keyexpr_t key, uint32_t id, uint8_t flags);
-#endif  // Z_FEATURE_INTEREST == 1
 
 #endif /* ZENOH_PICO_SESSION_INTEREST_H */
