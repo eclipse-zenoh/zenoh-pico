f0a157669f5fe121f147ce78f3141b9ed415c30a
Oussama Teffahi <oussama.teffahi@zettascale.tech>
2024-02-29 16:50:37 +0100

    Adjust z_sub_thr output

--- examples/unix/c11/z_sub_thr.c
+++ examples/unix/c11/z_sub_thr.c
@@ -53,8 +53,7 @@ void on_sample(const z_sample_t *sample, void *context) {
         // Stop set measurement
         stats->finished_rounds++;
         unsigned long elapsed_ms = z_clock_elapsed_ms(&stats->start);
-        printf("Received %d msg in %lu ms (%.1f msg/s)\n", PACKET_NB, elapsed_ms,
-               (double)(PACKET_NB * 1000) / (double)elapsed_ms);
+        printf("%f msg/s\n", (double)(PACKET_NB * 1000) / (double)elapsed_ms);
         stats->count = 0;
     }
 }
