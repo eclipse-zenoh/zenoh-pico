1f45c9d5a7075f2440102af6f6732a0935aa0748
Denis Biryukov <denis.biryukov@zettascale.tech>
2024-03-04 19:00:06 +0100

    put back previously removed zp_ functions as deprecated

---
+++ include/zenoh-pico/system/platform-common.h
@@ -0,0 +1,108 @@
+//
+// Copyright (c) 2022 ZettaScale Technology
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+//   Błażej Sowa, <blazej@fictionlab.pl>
+
+#ifndef ZENOH_PICO_SYSTEM_PLATFORM_COMMON_H
+#define ZENOH_PICO_SYSTEM_PLATFORM_COMMON_H
+
+#include <stdint.h>
+
+#include "zenoh-pico/config.h"
+
+#if defined(ZENOH_LINUX) || defined(ZENOH_MACOS) || defined(ZENOH_BSD)
+#include "zenoh-pico/system/platform/unix.h"
+#elif defined(ZENOH_WINDOWS)
+#include "zenoh-pico/system/platform/windows.h"
+#elif defined(ZENOH_ESPIDF)
+#include "zenoh-pico/system/platform/espidf.h"
+#elif defined(ZENOH_MBED)
+#include "zenoh-pico/system/platform/mbed.h"
+#elif defined(ZENOH_ZEPHYR)
+#include "zenoh-pico/system/platform/zephyr.h"
+#elif defined(ZENOH_ARDUINO_ESP32)
+#include "zenoh-pico/system/platform/arduino/esp32.h"
+#elif defined(ZENOH_ARDUINO_OPENCR)
+#include "zenoh-pico/system/platform/arduino/opencr.h"
+#elif defined(ZENOH_EMSCRIPTEN)
+#include "zenoh-pico/system/platform/emscripten.h"
+#elif defined(ZENOH_FLIPPER)
+#include "zenoh-pico/system/platform/flipper.h"
+#elif defined(ZENOH_FREERTOS_PLUS_TCP)
+#include "zenoh-pico/system/platform/freertos_plus_tcp.h"
+#else
+#include "zenoh-pico/system/platform/void.h"
+#error "Unknown platform"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*------------------ Random ------------------*/
+uint8_t z_random_u8(void);
+uint16_t z_random_u16(void);
+uint32_t z_random_u32(void);
+uint64_t z_random_u64(void);
+void z_random_fill(void *buf, size_t len);
+
+/*------------------ Memory ------------------*/
+void *z_malloc(size_t size);
+void *z_realloc(void *ptr, size_t size);
+void z_free(void *ptr);
+
+#if Z_FEATURE_MULTI_THREAD == 1
+/*------------------ Thread ------------------*/
+int8_t z_task_init(z_task_t *task, z_task_attr_t *attr, void *(*fun)(void *), void *arg);
+int8_t z_task_join(z_task_t *task);
+int8_t zp_task_cancel(z_task_t *task);
+void z_task_free(z_task_t **task);
+
+/*------------------ Mutex ------------------*/
+int8_t z_mutex_init(z_mutex_t *m);
+int8_t z_mutex_free(z_mutex_t *m);
+
+int8_t z_mutex_lock(z_mutex_t *m);
+int8_t z_mutex_trylock(z_mutex_t *m);
+int8_t z_mutex_unlock(z_mutex_t *m);
+
+/*------------------ CondVar ------------------*/
+int8_t z_condvar_init(z_condvar_t *cv);
+int8_t z_condvar_free(z_condvar_t *cv);
+
+int8_t z_condvar_signal(z_condvar_t *cv);
+int8_t z_condvar_wait(z_condvar_t *cv, z_mutex_t *m);
+#endif  // Z_FEATURE_MULTI_THREAD == 1
+
+/*------------------ Sleep ------------------*/
+int z_sleep_us(size_t time);
+int z_sleep_ms(size_t time);
+int z_sleep_s(size_t time);
+
+/*------------------ Clock ------------------*/
+z_clock_t z_clock_now(void);
+unsigned long z_clock_elapsed_us(z_clock_t *time);
+unsigned long z_clock_elapsed_ms(z_clock_t *time);
+unsigned long z_clock_elapsed_s(z_clock_t *time);
+
+/*------------------ Time ------------------*/
+z_time_t z_time_now(void);
+const char *z_time_now_as_str(char *const buf, unsigned long buflen);
+unsigned long z_time_elapsed_us(z_time_t *time);
+unsigned long z_time_elapsed_ms(z_time_t *time);
+unsigned long z_time_elapsed_s(z_time_t *time);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZENOH_PICO_SYSTEM_PLATFORM_COMMON_H */
