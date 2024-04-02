31e28a42bdc9c96469acbbbc39ed2ab141fada8b
Jean-Roland <jean.roland.gosse@gmail.com>
2024-04-02 16:06:17 +0200

    fix: missing function prototype warning

--- include/zenoh-pico/session/subscription.h
+++ include/zenoh-pico/session/subscription.h
@@ -17,12 +17,7 @@
 
 #include "zenoh-pico/net/session.h"
 
-#if Z_FEATURE_SUBSCRIPTION == 1
 /*------------------ Subscription ------------------*/
-_z_subscription_rc_t *_z_get_subscription_by_id(_z_session_t *zn, uint8_t is_local, const _z_zint_t id);
-_z_subscription_rc_list_t *_z_get_subscriptions_by_key(_z_session_t *zn, uint8_t is_local, const _z_keyexpr_t *keyexpr);
-
-_z_subscription_rc_t *_z_register_subscription(_z_session_t *zn, uint8_t is_local, _z_subscription_t *sub);
 void _z_trigger_local_subscriptions(_z_session_t *zn, const _z_keyexpr_t keyexpr, const uint8_t *payload,
                                     _z_zint_t payload_len, _z_n_qos_t qos
 #if Z_FEATURE_ATTACHMENT == 1
@@ -30,6 +25,12 @@ void _z_trigger_local_subscriptions(_z_session_t *zn, const _z_keyexpr_t keyexpr
                                     z_attachment_t att
 #endif
 );
+
+#if Z_FEATURE_SUBSCRIPTION == 1
+_z_subscription_rc_t *_z_get_subscription_by_id(_z_session_t *zn, uint8_t is_local, const _z_zint_t id);
+_z_subscription_rc_list_t *_z_get_subscriptions_by_key(_z_session_t *zn, uint8_t is_local, const _z_keyexpr_t *keyexpr);
+
+_z_subscription_rc_t *_z_register_subscription(_z_session_t *zn, uint8_t is_local, _z_subscription_t *sub);
 int8_t _z_trigger_subscriptions(_z_session_t *zn, const _z_keyexpr_t keyexpr, const _z_bytes_t payload,
                                 const _z_encoding_t encoding, const _z_zint_t kind, const _z_timestamp_t timestamp,
                                 const _z_n_qos_t qos
