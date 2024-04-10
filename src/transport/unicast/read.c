803195e5e923c9a63594c3bd3f26ac0aaa2bf542
Jean-Roland <jean.roland.gosse@gmail.com>
2024-04-10 15:24:47 +0200

    fix: concurrency issue on task_running init value

--- src/transport/unicast/read.c
+++ src/transport/unicast/read.c
@@ -124,13 +124,14 @@ void *_zp_unicast_read_task(void *ztu_arg) {
 int8_t _zp_unicast_start_read_task(_z_transport_t *zt, z_task_attr_t *attr, z_task_t *task) {
     // Init memory
     (void)memset(task, 0, sizeof(z_task_t));
+    zt->_transport._unicast._read_task_running = true;  // Init before z_task_init for concurrency issue
     // Init task
     if (z_task_init(task, attr, _zp_unicast_read_task, &zt->_transport._unicast) != _Z_RES_OK) {
+        zt->_transport._unicast._read_task_running = false;
         return _Z_ERR_SYSTEM_TASK_FAILED;
     }
     // Attach task
     zt->_transport._unicast._read_task = task;
-    zt->_transport._unicast._read_task_running = true;
     return _Z_RES_OK;
 }
 
