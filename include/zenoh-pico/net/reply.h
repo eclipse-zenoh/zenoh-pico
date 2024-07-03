65f39531d18c744199808cb71bee540e849dd309
Jean-Roland Gosse <jean.roland.gosse@gmail.com>
2024-07-03 14:55:16 +0200

    Move encoding value on receive side. (#490)
    * feat: change functions args to allow encoding move
    
    * feat: add z_value_move
    
    * feat: add string check in encoding_clear
    
    * feat: switch to encoding/value move
    
    * feat: pass pointer to encoding message field
    
    * fix: revert encoding_clear changes

--- include/zenoh-pico/net/reply.h
+++ include/zenoh-pico/net/reply.h
@@ -63,7 +63,7 @@ void _z_reply_clear(_z_reply_t *src);
 void _z_reply_free(_z_reply_t **hello);
 int8_t _z_reply_copy(_z_reply_t *dst, const _z_reply_t *src);
 _z_reply_t _z_reply_create(_z_keyexpr_t keyexpr, z_reply_tag_t tag, _z_id_t id, const _z_bytes_t payload,
-                           const _z_timestamp_t *timestamp, _z_encoding_t encoding, z_sample_kind_t kind,
+                           const _z_timestamp_t *timestamp, _z_encoding_t *encoding, z_sample_kind_t kind,
                            const _z_bytes_t attachment);
 
 typedef struct _z_pending_reply_t {
