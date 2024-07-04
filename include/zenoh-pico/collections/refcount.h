2d65e928436eee275ba8fd60fd81f1fbf3938453
Jean-Roland <jean.roland.gosse@gmail.com>
2024-07-04 12:03:31 +0200

    fix: rename refcount _cnt as strong count

--- include/zenoh-pico/collections/refcount.h
+++ include/zenoh-pico/collections/refcount.h
@@ -45,10 +45,10 @@
 
 // c11 atomic variant
 #define _ZP_RC_CNT_TYPE _z_atomic(unsigned int)
-#define _ZP_RC_OP_INIT_CNT _z_atomic_store_explicit(&p.in->_cnt, (unsigned int)1, _z_memory_order_relaxed);
-#define _ZP_RC_OP_INCR_CNT _z_atomic_fetch_add_explicit(&p->in->_cnt, (unsigned int)1, _z_memory_order_relaxed);
+#define _ZP_RC_OP_INIT_CNT _z_atomic_store_explicit(&p.in->_strong_cnt, (unsigned int)1, _z_memory_order_relaxed);
+#define _ZP_RC_OP_INCR_CNT _z_atomic_fetch_add_explicit(&p->in->_strong_cnt, (unsigned int)1, _z_memory_order_relaxed);
 #define _ZP_RC_OP_DECR_AND_CMP \
-    _z_atomic_fetch_sub_explicit(&p->in->_cnt, (unsigned int)1, _z_memory_order_release) > (unsigned int)1
+    _z_atomic_fetch_sub_explicit(&p->in->_strong_cnt, (unsigned int)1, _z_memory_order_release) > (unsigned int)1
 #define _ZP_RC_OP_SYNC atomic_thread_fence(_z_memory_order_acquire);
 
 #else  // ZENOH_C_STANDARD == 99
@@ -56,11 +56,11 @@
 
 // c99 gcc sync builtin variant
 #define _ZP_RC_CNT_TYPE unsigned int
-#define _ZP_RC_OP_INIT_CNT                              \
-    __sync_fetch_and_and(&p.in->_cnt, (unsigned int)0); \
-    __sync_fetch_and_add(&p.in->_cnt, (unsigned int)1);
-#define _ZP_RC_OP_INCR_CNT __sync_fetch_and_add(&p->in->_cnt, (unsigned int)1);
-#define _ZP_RC_OP_DECR_AND_CMP __sync_fetch_and_sub(&p->in->_cnt, (unsigned int)1) > (unsigned int)1
+#define _ZP_RC_OP_INIT_CNT                                     \
+    __sync_fetch_and_and(&p.in->_strong_cnt, (unsigned int)0); \
+    __sync_fetch_and_add(&p.in->_strong_cnt, (unsigned int)1);
+#define _ZP_RC_OP_INCR_CNT __sync_fetch_and_add(&p->in->_strong_cnt, (unsigned int)1);
+#define _ZP_RC_OP_DECR_AND_CMP __sync_fetch_and_sub(&p->in->_strong_cnt, (unsigned int)1) > (unsigned int)1
 #define _ZP_RC_OP_SYNC __sync_synchronize();
 
 #else  // !ZENOH_COMPILER_GCC
@@ -79,9 +79,9 @@
 
 // Single thread variant
 #define _ZP_RC_CNT_TYPE unsigned int
-#define _ZP_RC_OP_INIT_CNT p.in->_cnt = (unsigned int)1;
-#define _ZP_RC_OP_INCR_CNT p->in->_cnt += (unsigned int)1;
-#define _ZP_RC_OP_DECR_AND_CMP p->in->_cnt-- > (unsigned int)1
+#define _ZP_RC_OP_INIT_CNT p.in->_strong_cnt = (unsigned int)1;
+#define _ZP_RC_OP_INCR_CNT p->in->_strong_cnt += (unsigned int)1;
+#define _ZP_RC_OP_DECR_AND_CMP p->in->_strong_cnt-- > (unsigned int)1
 #define _ZP_RC_OP_SYNC
 
 #endif  // Z_FEATURE_MULTI_THREAD == 1
@@ -90,7 +90,7 @@
 #define _Z_REFCOUNT_DEFINE(name, type)                                                    \
     typedef struct name##_inner_rc_t {                                                    \
         type##_t val;                                                                     \
-        _ZP_RC_CNT_TYPE _cnt;                                                             \
+        _ZP_RC_CNT_TYPE _strong_cnt;                                                      \
     } name##_inner_rc_t;                                                                  \
     typedef struct name##_rc_t {                                                          \
         name##_inner_rc_t *in;                                                            \
