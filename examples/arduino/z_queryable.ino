d74f6ff334136dd37155da44471f98eea0457f18
Denis Biryukov <denis.biryukov@zettascale.tech>
2024-07-09 11:16:50 +0200

    typo fix

--- examples/arduino/z_queryable.ino
+++ examples/arduino/z_queryable.ino
@@ -43,7 +43,7 @@ void query_handler(const z_loaned_query_t *query, void *arg) {
     Serial.println("'");
 
     // Process value
-    z_view_string_t payload_string;
+    z_owned_string_t payload_string;
     z_bytes_deserialize_into_string(z_query_payload(query), &payload_string);
     if (z_string_len(z_string_loan(&payload_string)) > 1) {
         Serial.print("     with value '");
