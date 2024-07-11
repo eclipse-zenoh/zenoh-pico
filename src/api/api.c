d266abaa61cd595af9d7116ede5b44b252d8df53
Denis Biryukov <denis.biryukov@zettascale.tech>
2024-07-11 17:41:06 +0200

    revert z_keyexpr_as_view_string_check

--- src/api/api.c
+++ src/api/api.c
@@ -108,7 +108,7 @@ int8_t z_keyexpr_as_view_string(const z_loaned_keyexpr_t *keyexpr, z_view_string
         ret = _Z_ERR_GENERIC;
     }
 
-    return _Z_RES_OK;
+    return ret;
 }
 
 int8_t z_keyexpr_concat(z_owned_keyexpr_t *key, const z_loaned_keyexpr_t *left, const char *right, size_t len) {
