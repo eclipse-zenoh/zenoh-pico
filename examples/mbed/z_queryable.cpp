e59e3f734c7343aec838b4f68641838605e77602
Denis Biryukov <denis.biryukov@zettascale.tech>
2024-07-30 14:02:16 +0200

    fix examples

--- examples/mbed/z_queryable.cpp
+++ examples/mbed/z_queryable.cpp
@@ -37,8 +37,10 @@ void query_handler(const z_loaned_query_t *query, void *ctx) {
     z_keyexpr_as_view_string(z_query_keyexpr(query), &keystr);
     z_view_string_t pred;
     z_query_parameters(query, &pred);
-    printf(" >> [Queryable handler] Received Query '%s%.*s'\n", z_string_data(z_view_string_loan(&keystr)),
-           (int)z_view_string_loan(&pred)->len, z_view_string_loan(&pred)->val);
+    printf(" >> [Queryable handler] Received Query '%.*s%.*s'\n", 
+        (int)z_string_len(z_loan(keystr)), z_string_data(z_loan(keystr)), 
+        (int)z_string_len(z_loan(params)), z_string_data(z_loan(params))
+    );
     // Process value
     z_owned_string_t payload_string;
     z_bytes_deserialize_into_string(z_query_payload(query), &payload_string);
--- examples/zephyr/z_queryable.c
+++ examples/zephyr/z_queryable.c
@@ -37,8 +37,8 @@ void query_handler(const z_loaned_query_t *query, void *ctx) {
     z_keyexpr_as_view_string(z_query_keyexpr(query), &keystr);
     z_view_string_t params;
     z_query_parameters(query, &params);
-    printf(" >> [Queryable handler] Received Query '%s%.*s'\n", z_string_data(z_loan(keystr)), z_loan(params)->len,
-           z_loan(params)->val);
+    printf(" >> [Queryable handler] Received Query '%.*s%.*s'\n", (int)z_string_len(z_loan(keystr)),
+           z_string_data(z_loan(keystr)), (int)z_string_len(z_loan(params)), z_string_data(z_loan(params)));
     // Process value
     z_owned_string_t payload_string;
     z_bytes_deserialize_into_string(z_query_payload(query), &payload_string);
