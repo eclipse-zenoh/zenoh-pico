7407dcc03da3fcda933a167d1e030ea07eb055fc
Jean-Roland <jean.roland.gosse@gmail.com>
2024-09-17 14:44:01 +0200

    refactor: move encoding constants to encoding.h

--- include/zenoh-pico.h
+++ include/zenoh-pico.h
@@ -24,6 +24,7 @@
 #define ZENOH_PICO_TWEAK 0
 
 #include "zenoh-pico/api/constants.h"
+#include "zenoh-pico/api/encoding.h"
 #include "zenoh-pico/api/handlers.h"
 #include "zenoh-pico/api/macros.h"
 #include "zenoh-pico/api/primitives.h"
