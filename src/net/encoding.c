a28445fadd3365e409c8e0706e394722cfb4ede1
Jean-Roland <jean.roland.gosse@gmail.com>
2024-10-10 16:04:21 +0200

    feat: skip null string copy in encoding

--- src/net/encoding.c
+++ src/net/encoding.c
@@ -41,9 +41,12 @@ z_result_t _z_encoding_make(_z_encoding_t *encoding, uint16_t id, const char *sc
 void _z_encoding_clear(_z_encoding_t *encoding) { _z_string_clear(&encoding->schema); }
 
 z_result_t _z_encoding_copy(_z_encoding_t *dst, const _z_encoding_t *src) {
-    *dst = _z_encoding_null();
-    _Z_RETURN_IF_ERR(_z_string_copy(&dst->schema, &src->schema));
     dst->id = src->id;
+    if (_z_string_check(&src->schema)) {
+        _Z_RETURN_IF_ERR(_z_string_copy(&dst->schema, &src->schema));
+    } else {
+        dst->schema = _z_string_null();
+    }
     return _Z_RES_OK;
 }
 
