6ca364b748b86fe5b16b8dcb673204baedc82456
Alexander Bushnev <Alexander@Bushnev.pro>
2024-10-22 19:03:40 +0200

    Fix read/lease task cleanup (#760)

--- src/transport/unicast/transport.c
+++ src/transport/unicast/transport.c
@@ -281,11 +281,11 @@ void _z_unicast_transport_clear(_z_transport_t *zt) {
     // Clean up tasks
     if (ztu->_read_task != NULL) {
         _z_task_join(ztu->_read_task);
-        _z_task_free(&ztu->_read_task);
+        z_free(ztu->_read_task);
     }
     if (ztu->_lease_task != NULL) {
         _z_task_join(ztu->_lease_task);
-        _z_task_free(&ztu->_lease_task);
+        z_free(ztu->_lease_task);
     }
 
     // Clean up the mutexes
