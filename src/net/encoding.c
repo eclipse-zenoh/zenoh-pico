e989139df433608b353abbec5cb1b359d4e25eec
Jean-Roland <jean.roland.gosse@gmail.com>
2024-10-28 20:47:46 +0100

    feat: check before encoding clear

--- src/net/encoding.c
+++ src/net/encoding.c
@@ -38,7 +38,11 @@ z_result_t _z_encoding_make(_z_encoding_t *encoding, uint16_t id, const char *sc
     return _Z_RES_OK;
 }
 
-void _z_encoding_clear(_z_encoding_t *encoding) { _z_string_clear(&encoding->schema); }
+void _z_encoding_clear(_z_encoding_t *encoding) {
+    if (_z_string_check(&encoding->schema)) {
+        _z_string_clear(&encoding->schema);
+    }
+}
 
 z_result_t _z_encoding_copy(_z_encoding_t *dst, const _z_encoding_t *src) {
     dst->id = src->id;
