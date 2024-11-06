f4183a96e2a440cf7ee21458d313e1a9bcca6610
Jean-Roland <jean.roland.gosse@gmail.com>
2024-11-06 18:22:08 +0100

    feat: rework svec expand

--- src/collections/vec.c
+++ src/collections/vec.c
@@ -135,7 +135,7 @@ void _z_vec_remove(_z_vec_t *v, size_t pos, z_element_free_f free_f) {
 
 /*-------- svec --------*/
 _z_svec_t _z_svec_make(size_t capacity, size_t element_size) {
-    _z_svec_t v = {._capacity = 0, ._len = 0, ._val = NULL};
+    _z_svec_t v = _z_svec_null();
     if (capacity != 0) {
         v._val = z_malloc(element_size * capacity);
     }
@@ -226,13 +226,13 @@ bool _z_svec_is_empty(const _z_svec_t *v) { return v->_len == 0; }
 
 z_result_t _z_svec_expand(_z_svec_t *v, z_element_move_f move, size_t element_size, bool use_elem_f) {
     // Allocate a new vector
-    size_t _capacity = v->_capacity == 0 ? 1 : (v->_capacity << 1);
+    size_t _capacity = v->_capacity << 1;
     void *_val = (void *)z_malloc(_capacity * element_size);
     if (_val == NULL) {
         return _Z_ERR_SYSTEM_OUT_OF_MEMORY;
     }
+    // Move and clear old data
     __z_svec_move_inner(_val, v->_val, move, v->_len, element_size, use_elem_f);
-    // Free the old data
     z_free(v->_val);
     // Update the current vector
     v->_val = _val;
@@ -241,7 +241,9 @@ z_result_t _z_svec_expand(_z_svec_t *v, z_element_move_f move, size_t element_si
 }
 
 z_result_t _z_svec_append(_z_svec_t *v, const void *e, z_element_move_f move, size_t element_size, bool use_elem_f) {
-    if (v->_len == v->_capacity) {
+    if (v->_capacity == 0) {
+        *v = _z_svec_make(1, element_size);
+    } else if (v->_len == v->_capacity) {
         _Z_RETURN_IF_ERR(_z_svec_expand(v, move, element_size, use_elem_f));
     }
     // Append element
