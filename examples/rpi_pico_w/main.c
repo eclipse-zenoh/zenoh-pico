10cc5f044d37aa23023583d8380f9e8bc985b3b8
Alexander Bushnev <Alexander@Bushnev.pro>
2024-12-03 00:44:27 +0100

    Cleanup main module

--- examples/rpi_pico_w/main.c
+++ examples/rpi_pico_w/main.c
@@ -1,14 +1,7 @@
 #include "FreeRTOS.h"
-#include "lwip/apps/lwiperf.h"
-#include "lwip/ip4_addr.h"
-#include "lwip/netif.h"
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
 #include "task.h"
-
-#ifndef RUN_FREERTOS_ON_CORE
-#define RUN_FREERTOS_ON_CORE 0
-#endif
 
 #define TEST_TASK_PRIORITY (tskIDLE_PRIORITY + 2UL)
 
@@ -23,7 +16,7 @@ void main_task(__unused void *params) {
     printf("Connecting to Wi-Fi...\n");
     if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
         printf("failed to connect.\n");
-        exit(1);
+        vTaskDelete(NULL);
     } else {
         printf("Connected.\n");
     }
@@ -34,40 +27,15 @@ void main_task(__unused void *params) {
 
     cyw43_arch_deinit();
 
-    while (true) {
-        vTaskDelay(100);
-    }
-}
-
-void vLaunch(void) {
-    TaskHandle_t task;
-    xTaskCreate(main_task, "TestMainThread", configMINIMAL_STACK_SIZE * 8, NULL, TEST_TASK_PRIORITY, &task);
-
-    /* Start the tasks and timer running. */
-    vTaskStartScheduler();
+    vTaskDelete(NULL);
 }
 
 int main(void) {
     stdio_init_all();
 
-    /* Configure the hardware ready to run the demo. */
-    const char *rtos_name;
-#if (configNUMBER_OF_CORES > 1)
-    rtos_name = "FreeRTOS SMP";
-#else
-    rtos_name = "FreeRTOS";
-#endif
+    xTaskCreate(main_task, "TestMainThread", configMINIMAL_STACK_SIZE * 16, NULL, TEST_TASK_PRIORITY, NULL);
 
-#if (configNUMBER_OF_CORES == 2)
-    printf("Starting %s on both cores:\n", rtos_name);
-    vLaunch();
-#elif (RUN_FREERTOS_ON_CORE == 1)
-    printf("Starting %s on core 1:\n", rtos_name);
-    multicore_launch_core1(vLaunch);
-    while (true);
-#else
-    printf("Starting %s on core 0:\n", rtos_name);
-    vLaunch();
-#endif
+    vTaskStartScheduler();
+
     return 0;
 }
