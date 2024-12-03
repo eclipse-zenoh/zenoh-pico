fdec98e20acbe1380430ca1608154a0b77aa6980
Alexander Bushnev <Alexander@Bushnev.pro>
2024-12-03 16:36:59 +0100

    Add some debug output to main

--- examples/rpi_pico_w/main.c
+++ examples/rpi_pico_w/main.c
@@ -7,21 +7,32 @@
 
 int app_main();
 
+void print_ip_address() {
+    struct netif *netif = &cyw43_state.netif[CYW43_ITF_STA];
+    if (netif_is_up(netif)) {
+        printf("IP Address: %s\n", ip4addr_ntoa(netif_ip4_addr(netif)));
+        printf("Netmask: %s\n", ip4addr_ntoa(netif_ip4_netmask(netif)));
+        printf("Gateway: %s\n", ip4addr_ntoa(netif_ip4_gw(netif)));
+    } else {
+        printf("Network interface is down.\n");
+    }
+}
+
 void main_task(__unused void *params) {
     if (cyw43_arch_init()) {
-        printf("failed to initialise\n");
+        printf("Failed to initialise\n");
         return;
     }
+
     cyw43_arch_enable_sta_mode();
     printf("Connecting to Wi-Fi...\n");
-    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
-        printf("failed to connect.\n");
-        vTaskDelete(NULL);
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000) == 0) {
+        printf("Wi-Fi connected.\n");
+        print_ip_address();
+        app_main();
     } else {
-        printf("Connected.\n");
+        printf("Failed to connect Wi-Fi\n");
     }
-
-    app_main();
 
     printf("Terminate.\n");
 
