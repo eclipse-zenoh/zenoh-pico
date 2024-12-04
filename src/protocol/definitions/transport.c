bca36bd80989d40ab13dc011f5658f9b160e066a
Joseph Perez <joperez@hotmail.fr>
2024-12-04 13:06:30 +0100

    fix: apply PR feedbacks

--- src/protocol/definitions/transport.c
+++ src/protocol/definitions/transport.c
@@ -150,13 +150,10 @@ _z_transport_message_t _z_t_msg_make_init_syn(z_whatami_t whatami, _z_id_t zid) 
     }
 
 #if Z_FEATURE_FRAGMENTATION == 1
-    bool has_patch = msg._body._join._patch != _Z_NO_PATCH;
-#else
-    bool has_patch = false;
-#endif
-    if (has_patch) {
+    if (msg._body._init._patch != _Z_NO_PATCH) {
         _Z_SET_FLAG(msg._header, _Z_FLAG_T_Z);
     }
+#endif
 
     return msg;
 }
