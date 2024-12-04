e997fc468c9ab960d63edfeabea5e55dfd1268a8
Joseph Perez <joperez@hotmail.fr>
2024-12-04 11:07:37 +0100

    Update src/transport/unicast/rx.c
    Co-authored-by: Alexander Bushnev <sashacmc@gmail.com>

--- src/transport/unicast/rx.c
+++ src/transport/unicast/rx.c
@@ -146,7 +146,7 @@ z_result_t _z_unicast_handle_transport_message(_z_transport_unicast_t *ztu, _z_t
             if (_Z_HAS_FLAG(t_msg->_header, _Z_FLAG_T_FRAME_R) == true) {
                 // @TODO: amend once reliability is in place. For the time being only
                 //        monotonic SNs are ensured
-                if (_z_sn_precedes(ztu->_sn_res, ztu->_sn_rx_reliable, t_msg->_body._frame._sn) == true) {
+                if (_z_sn_precedes(ztu->_sn_res, ztu->_sn_rx_reliable, t_msg->_body._frame._sn)) {
                     bool consecutive =
                         _z_sn_consecutive(ztu->_sn_res, ztu->_sn_rx_reliable, t_msg->_body._fragment._sn);
                     ztu->_sn_rx_reliable = t_msg->_body._frame._sn;
