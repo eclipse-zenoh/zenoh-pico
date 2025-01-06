0815ef308ac5efa5e32f09db322d776eafe6b45e
Jean-Roland <jean.roland.gosse@gmail.com>
2025-01-06 16:14:52 +0100

    feat: remove batch api from unstable

--- include/zenoh-pico/api/primitives.h
+++ include/zenoh-pico/api/primitives.h
@@ -2077,7 +2077,6 @@ z_result_t z_declare_background_subscriber(const z_loaned_session_t *zs, const z
 const z_loaned_keyexpr_t *z_subscriber_keyexpr(const z_loaned_subscriber_t *subscriber);
 #endif
 
-#ifdef Z_FEATURE_UNSTABLE_API
 #if Z_FEATURE_BATCHING == 1
 /**
  * Activate the batching mechanism, any message that would have been sent on the network by a subsequent api call (e.g
@@ -2113,7 +2112,6 @@ z_result_t zp_batch_flush(const z_loaned_session_t *zs);
  *   ``0`` if batching stopped and batch successfully sent, ``negative value`` otherwise.
  */
 z_result_t zp_batch_stop(const z_loaned_session_t *zs);
-#endif
 #endif
 
 /************* Multi Thread Tasks helpers **************/
