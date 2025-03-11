44e30f8d12adec49ae809ec80a2c9ff0c9364f06
Jean-Roland <jean.roland.gosse@gmail.com>
2025-03-11 11:04:45 +0100

    fix: endpoint memory leak

--- src/link/link.c
+++ src/link/link.c
@@ -23,13 +23,16 @@
 
 z_result_t _z_open_socket(const _z_string_t *locator, _z_sys_net_socket_t *socket) {
     _z_endpoint_t ep;
+    z_result_t ret = _Z_RES_OK;
     _Z_RETURN_IF_ERR(_z_endpoint_from_string(&ep, locator));
     // For now only tcp endpoints are supported
     if (_z_endpoint_tcp_valid(&ep) == _Z_RES_OK) {
-        return _z_new_peer_tcp(&ep, socket);
+        ret = _z_new_peer_tcp(&ep, socket);
     } else {
-        return _Z_ERR_GENERIC;
+        ret = _Z_ERR_GENERIC;
     }
+    _z_endpoint_clear(&ep);
+    return ret;
 }
 
 z_result_t _z_open_link(_z_link_t *zl, const _z_string_t *locator) {
