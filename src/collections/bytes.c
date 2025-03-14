57a5f4e8b8bcfc335518c7a5df596a81b2375215
Jean-Roland <jean.roland.gosse@gmail.com>
2025-03-14 15:48:51 +0100

    fix: null string allocation issue

--- src/collections/bytes.c
+++ src/collections/bytes.c
@@ -104,7 +104,10 @@ z_result_t _z_bytes_from_slice(_z_bytes_t *b, _z_slice_t s) {
 
 z_result_t _z_bytes_from_buf(_z_bytes_t *b, const uint8_t *src, size_t len) {
     *b = _z_bytes_null();
-    _z_slice_t s = _z_slice_copy_from_buf(src, len);
+    _z_slice_t s = _z_slice_null();
+    if (len > 0) {
+        s = _z_slice_copy_from_buf(src, len);
+    }
     if (s.len != len) return _Z_ERR_SYSTEM_OUT_OF_MEMORY;
     return _z_bytes_from_slice(b, s);
 }
