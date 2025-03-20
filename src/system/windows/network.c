360f2dbbaa1b4a9dfd49f839a559111fa30f6e70
Jean-Roland <jean.roland.gosse@gmail.com>
2025-03-20 16:25:01 +0100

    ft: add listen_tcp for other platforms

--- src/system/windows/network.c
+++ src/system/windows/network.c
@@ -208,12 +208,72 @@ z_result_t _z_open_tcp(_z_sys_net_socket_t *sock, const _z_sys_net_endpoint_t re
 
 z_result_t _z_listen_tcp(_z_sys_net_socket_t *sock, const _z_sys_net_endpoint_t lep) {
     z_result_t ret = _Z_RES_OK;
-    (void)sock;
-    (void)lep;
 
-    // @TODO: To be implemented
-    ret = _Z_ERR_GENERIC;
+    // Initialize Winsock
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        return _Z_ERR_GENERIC;
+    }
 
+    // Open socket
+    *sock = socket(lep->ai_family, lep->ai_socktype, lep->ai_protocol);
+    if (*sock == INVALID_SOCKET) {
+        WSACleanup();
+        return _Z_ERR_GENERIC;
+    }
+
+    // Set options
+    int value = true;
+    if ((ret == _Z_RES_OK) && (setsockopt(*sock, SOL_SOCKET, SO_REUSEADDR, (const char *)&value, sizeof(value)) < 0)) {
+        ret = _Z_ERR_GENERIC;
+    }
+
+    int flags = 1;
+    if ((ret == _Z_RES_OK) && (setsockopt(*sock, SOL_SOCKET, SO_KEEPALIVE, (const char *)&flags, sizeof(flags)) < 0)) {
+        ret = _Z_ERR_GENERIC;
+    }
+
+#if Z_FEATURE_TCP_NODELAY == 1
+    if ((ret == _Z_RES_OK) && (setsockopt(*sock, IPPROTO_TCP, TCP_NODELAY, (const char *)&flags, sizeof(flags)) < 0)) {
+        ret = _Z_ERR_GENERIC;
+    }
+#endif
+
+    struct linger ling;
+    ling.l_onoff = 1;
+    ling.l_linger = Z_TRANSPORT_LEASE / 1000;
+    if ((ret == _Z_RES_OK) &&
+        (setsockopt(*sock, SOL_SOCKET, SO_LINGER, (const char *)&ling, sizeof(struct linger)) < 0)) {
+        ret = _Z_ERR_GENERIC;
+    }
+
+    if (ret != _Z_RES_OK) {
+        closesocket(*sock);
+        WSACleanup();
+        return ret;
+    }
+
+    struct addrinfo *it = NULL;
+    for (it = lep; it != NULL; it = it->ai_next) {
+        if (bind(*sock, it->ai_addr, it->ai_addrlen) < 0) {
+            if (it->ai_next == NULL) {
+                ret = _Z_ERR_GENERIC;
+                break;
+            }
+        }
+        if (listen(*sock, Z_LISTEN_MAX_CONNECTION_NB) < 0) {
+            if (it->ai_next == NULL) {
+                ret = _Z_ERR_GENERIC;
+                break;
+            }
+        }
+    }
+
+    if (ret != _Z_RES_OK) {
+        closesocket(*sock);
+    }
+
+    // Cleanup Winsock
+    WSACleanup();
     return ret;
 }
 
