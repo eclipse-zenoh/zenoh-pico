a26e7496e7cb142b3648c2be664cdb2bf1f9c4ca
Jean-Roland <jean.roland.gosse@gmail.com>
2025-05-19 10:45:37 +0200

    fix: deactivate write filter on multicast

--- src/api/api.c
+++ src/api/api.c
@@ -1044,14 +1044,18 @@ z_result_t z_declare_publisher(const z_loaned_session_t *zs, z_owned_publisher_t
     // Set publisher
     _z_publisher_t int_pub = _z_declare_publisher(zs, key, opt.encoding == NULL ? NULL : &opt.encoding->_this._val,
                                                   opt.congestion_control, opt.priority, opt.is_express, reliability);
-    // Create write filter
-    z_result_t res =
-        _z_write_filter_create(_Z_RC_IN_VAL(zs), &int_pub._filter, keyexpr_aliased, _Z_INTEREST_FLAG_SUBSCRIBERS);
-    if (res != _Z_RES_OK) {
-        if (key._id != Z_RESOURCE_ID_NONE) {
-            _z_undeclare_resource(_Z_RC_IN_VAL(zs), key._id);
+
+    // TODO: Implement interest protocol for multicast transports
+    if (_Z_RC_IN_VAL(zs)->_tp._type == _Z_TRANSPORT_UNICAST_TYPE) {
+        // Create write filter
+        z_result_t res =
+            _z_write_filter_create(_Z_RC_IN_VAL(zs), &int_pub._filter, keyexpr_aliased, _Z_INTEREST_FLAG_SUBSCRIBERS);
+        if (res != _Z_RES_OK) {
+            if (key._id != Z_RESOURCE_ID_NONE) {
+                _z_undeclare_resource(_Z_RC_IN_VAL(zs), key._id);
+            }
+            return res;
         }
-        return res;
     }
     pub->_val = int_pub;
     return _Z_RES_OK;
@@ -1393,14 +1397,17 @@ z_result_t z_declare_querier(const z_loaned_session_t *zs, z_owned_querier_t *qu
     _z_querier_t int_querier = _z_declare_querier(zs, key, opt.consolidation.mode, opt.congestion_control, opt.target,
                                                   opt.priority, opt.is_express, opt.timeout_ms,
                                                   opt.encoding == NULL ? NULL : &opt.encoding->_this._val, reliability);
-    // Create write filter
-    z_result_t res =
-        _z_write_filter_create(_Z_RC_IN_VAL(zs), &int_querier._filter, keyexpr_aliased, _Z_INTEREST_FLAG_QUERYABLES);
-    if (res != _Z_RES_OK) {
-        if (key._id != Z_RESOURCE_ID_NONE) {
-            _z_undeclare_resource(_Z_RC_IN_VAL(zs), key._id);
+    // TODO: Implement interest protocol for multicast transports
+    if (_Z_RC_IN_VAL(zs)->_tp._type == _Z_TRANSPORT_UNICAST_TYPE) {
+        // Create write filter
+        z_result_t res = _z_write_filter_create(_Z_RC_IN_VAL(zs), &int_querier._filter, keyexpr_aliased,
+                                                _Z_INTEREST_FLAG_QUERYABLES);
+        if (res != _Z_RES_OK) {
+            if (key._id != Z_RESOURCE_ID_NONE) {
+                _z_undeclare_resource(_Z_RC_IN_VAL(zs), key._id);
+            }
+            return res;
         }
-        return res;
     }
     querier->_val = int_querier;
     return _Z_RES_OK;
