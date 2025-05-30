7fc55fd6e04dc49e0809ffe777d15543e75930db
Gasper Jersin <gjersin89@gmail.com>
2025-05-30 12:29:10 +0200

    Add STM32 Threadx system with serial device using circular DMA.

---
+++ src/system/threadx/stm32/network.c
@@ -0,0 +1,232 @@
+/* Ubiquity robotics
+ * ======================================================================
+ * Zenoh-pico stm32 threadx
+ * Network implementation for serial device running in circular DMA mode.
+ * ======================================================================
+ */
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
+
+#include "zenoh-pico/collections/slice.h"
+#include "zenoh-pico/collections/string.h"
+#include "zenoh-pico/config.h"
+#include "zenoh-pico/protocol/codec/serial.h"
+#include "zenoh-pico/system/common/serial.h"
+#include "zenoh-pico/system/link/serial.h"
+#include "zenoh-pico/system/platform.h"
+#include "zenoh-pico/utils/checksum.h"
+#include "zenoh-pico/utils/encoding.h"
+#include "zenoh-pico/utils/logging.h"
+#include "zenoh-pico/utils/pointers.h"
+
+#include "hal.h"
+
+#if Z_FEATURE_LINK_TCP == 1
+#error "Z_FEATURE_LINK_TCP is not supported"
+#endif
+
+#if Z_FEATURE_LINK_BLUETOOTH == 1
+#error "Z_FEATURE_LINK_BLUETOOTH is not supported"
+#endif
+
+#if Z_FEATURE_RAWETH_TRANSPORT == 1
+#error "Z_FEATURE_RAWETH_TRANSPORT is not supported"
+#endif
+
+#if Z_FEATURE_LINK_SERIAL == 1
+
+#define RX_DMA_BUFFER_SIZE (_Z_SERIAL_MAX_COBS_BUF_SIZE * 2 + 2) // for 2 max sized COBS frames + something little extra
+static uint8_t dma_buffer[RX_DMA_BUFFER_SIZE];
+static uint16_t delimiter_offset = 0;
+static TX_SEMAPHORE data_ready_semaphore, data_processing_semaphore;
+extern UART_HandleTypeDef ZENOH_HUART;
+/*------------------ Serial sockets ------------------*/
+void _z_socket_close(_z_sys_net_socket_t* sock) { _ZP_UNUSED(sock); }
+
+z_result_t _z_open_serial_from_pins(_z_sys_net_socket_t *sock, uint32_t txpin, uint32_t rxpin, uint32_t baudrate) {
+    z_result_t ret = _Z_RES_OK;
+    (void)(sock);
+    (void)(txpin);
+    (void)(rxpin);
+    (void)(baudrate);
+
+    // Not implemented
+    ret = _Z_ERR_GENERIC;
+
+    return ret;
+}
+
+z_result_t _z_open_serial_from_dev(_z_sys_net_socket_t *sock, char *dev, uint32_t baudrate) {
+	(void)(dev);
+	(void)(baudrate);
+
+	tx_semaphore_create(&data_ready_semaphore, "Data Ready", 0);
+	tx_semaphore_create(&data_processing_semaphore, "Data Processing", 1); // get is 1 step signal ahead, 0-still processing, 1-ready to process new data
+	HAL_UARTEx_ReceiveToIdle_DMA(&ZENOH_HUART, (uint8_t*)dma_buffer, RX_DMA_BUFFER_SIZE);
+	return _z_connect_serial(*sock);
+}
+
+z_result_t _z_listen_serial_from_pins(_z_sys_net_socket_t *sock, uint32_t txpin, uint32_t rxpin, uint32_t baudrate) {
+    z_result_t ret = _Z_RES_OK;
+    (void)(sock);
+    (void)(txpin);
+    (void)(rxpin);
+    (void)(baudrate);
+
+    // Not implemented
+    ret = _Z_ERR_GENERIC;
+
+    return ret;
+}
+
+z_result_t _z_listen_serial_from_dev(_z_sys_net_socket_t *sock, char *dev, uint32_t baudrate) {
+    z_result_t ret = _Z_RES_OK;
+    (void)(sock);
+    (void)(dev);
+    (void)(baudrate);
+
+    // Not implemented
+    ret = _Z_ERR_GENERIC;
+
+    return ret;
+}
+
+void _z_close_serial(_z_sys_net_socket_t *sock) {
+	// Nothing to do
+}
+
+size_t _z_read_serial_internal(const _z_sys_net_socket_t sock, uint8_t *header, uint8_t *ptr, size_t len) {
+    size_t rb = 0;
+
+	static uint16_t last_offset = 0;
+
+	// Block on data ready for one second only - TODO: check if time is ok
+	/* We used to block forever but like this zenoh-init never resends initial
+	 * handshake to router because this function never returns.
+	 * This is a problem if router starts later than this node.
+	 */
+	if (tx_semaphore_get(&data_ready_semaphore, TX_TIMER_TICKS_PER_SECOND) != TX_SUCCESS){
+	    return 0;
+	}
+
+	// COMUNICATION
+	if (delimiter_offset < last_offset){
+	    // wrap around
+	    rb = (RX_DMA_BUFFER_SIZE - last_offset) + delimiter_offset;
+	}
+	else{
+	    rb = delimiter_offset - last_offset;
+	}
+	if (rb >= _Z_SERIAL_MAX_COBS_BUF_SIZE) {
+		_Z_ERROR("Received COBS message exceeds _Z_SERIAL_MAX_COBS_BUF_SIZE!");
+		return SIZE_MAX;
+	}
+
+    uint8_t *raw_buf = z_malloc(rb);
+    if(raw_buf == NULL){
+        return 0;
+    }
+    if (delimiter_offset < last_offset){
+        // copy in two parts if dma wrapped around
+        size_t s2 = RX_DMA_BUFFER_SIZE - last_offset;
+        memcpy(raw_buf, dma_buffer + last_offset, s2);
+        memcpy(raw_buf+s2, dma_buffer, delimiter_offset);
+
+    }
+    else{
+        memcpy(raw_buf, dma_buffer + last_offset, rb);
+    }
+	last_offset = delimiter_offset;
+
+	// resume recieving data
+	tx_semaphore_put(&data_processing_semaphore);
+
+    uint8_t *tmp_buf = z_malloc(_Z_SERIAL_MFS_SIZE);
+    if(tmp_buf == NULL){
+        z_free(raw_buf);
+        return 0;
+    }
+    size_t ret = _z_serial_msg_deserialize(raw_buf, rb, ptr, len, header, tmp_buf, _Z_SERIAL_MFS_SIZE);
+
+    z_free(raw_buf);
+    z_free(tmp_buf);
+
+    return ret;
+}
+
+size_t _z_send_serial_internal(const _z_sys_net_socket_t sock, uint8_t header, const uint8_t *ptr, size_t len) {
+    uint8_t *tmp_buf = (uint8_t *)z_malloc(_Z_SERIAL_MFS_SIZE);
+    if(tmp_buf == NULL) {
+        return SIZE_MAX;
+    }
+    uint8_t *raw_buf = (uint8_t *)z_malloc(_Z_SERIAL_MAX_COBS_BUF_SIZE);
+    if(raw_buf == NULL) {
+        z_free(tmp_buf);
+        return SIZE_MAX;
+    }
+    size_t ret = _z_serial_msg_serialize(raw_buf, _Z_SERIAL_MAX_COBS_BUF_SIZE, ptr, len, header, tmp_buf, _Z_SERIAL_MFS_SIZE);
+
+    if (ret == SIZE_MAX) {
+        z_free(raw_buf);
+        z_free(tmp_buf);
+        return ret;
+    }
+
+    if (HAL_UART_Transmit(&ZENOH_HUART, raw_buf, ret, 2000) != HAL_OK) {
+		_Z_ERROR("Could not send to serial device!");
+		ret = SIZE_MAX;
+	}
+
+    z_free(raw_buf);
+    z_free(tmp_buf);
+
+    return len;
+}
+
+
+void zptxstm32_rx_event_cb(UART_HandleTypeDef *huart, uint16_t offset){
+    static uint16_t last_offset = 0;
+    if (huart != &ZENOH_HUART){
+        // not correct uart
+        return;
+    }
+    if (offset == last_offset)
+        // ignore if called twice (this happens on every half buffer)
+        return;
+
+    if (offset < last_offset)
+        // if wrap around reset last_size
+        last_offset = 0;
+
+    while (last_offset < offset) {
+        if (dma_buffer[last_offset] == (uint8_t)0x00) {
+            tx_semaphore_get(&data_processing_semaphore, TX_WAIT_FOREVER); // Block if data isnt processed yet
+            delimiter_offset = last_offset + 1;
+            tx_semaphore_put(&data_ready_semaphore); // Notify waiting task
+
+        }
+        ++last_offset;
+    }
+}
+
+void zptxstm32_error_event_cb(UART_HandleTypeDef *huart){
+	_Z_ERROR("UART error!");
+}
+
+
+#if ZENOH_THREADX_STM32_GEN_IRQ == 1
+// This callback triggers when there is certain amount of "idling" on the UART line
+// ("idling" == 1.5 stop bits; it also triggers when the buffer is half full)
+// Tremendously decreases number of interrupts needed for communication
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t offset) {
+    zptxstm32_rx_event_cb(huart, offset);
+}
+
+void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart) {
+    zptxstm32_error_event_cb(huart);
+}
+#endif
+
+#endif //Z_FEATURE_LINK_SERIAL
---
+++ src/system/threadx/stm32/system.c
@@ -0,0 +1,362 @@
+/* Ubiquity robotics
+ * =============================================================
+ * Zenoh-pico stm32 threadx
+ * System implementation for threadx.
+ * =============================================================
+ */
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "tx_api.h"
+#include "app_threadx.h"
+#include "zenoh-pico/config.h"
+#include "zenoh-pico/system/platform.h"
+#include "zenoh-pico.h"
+
+/* pointer to threadx byte pool, should be provided by application */
+extern TX_BYTE_POOL* pthreadx_byte_pool;
+
+/*------------------ Random ------------------*/
+uint8_t z_random_u8(void) { return z_random_u32(); }
+
+uint16_t z_random_u16(void) { return z_random_u32(); }
+
+uint32_t z_random_u32(void) { return random(); }
+
+
+uint64_t z_random_u64(void) {
+    uint64_t ret = 0;
+    ret |= z_random_u32();
+    ret = ret << 32;
+    ret |= z_random_u32();
+    return ret;
+}
+
+void z_random_fill(void *buf, size_t len) {
+    for (size_t i = 0; i < len; i++) {
+        ((uint8_t *)buf)[i] = z_random_u8();
+    }
+}
+
+/*------------------ Memory ------------------*/
+void *z_malloc(size_t size) {
+	void * ptr = NULL;
+
+	uint8_t r = tx_byte_allocate(pthreadx_byte_pool, &ptr, size, TX_WAIT_FOREVER);
+	if(r != TX_SUCCESS)
+	{
+		ptr = NULL;
+	}
+	return ptr;
+}
+
+void *z_realloc(void *ptr, size_t size) {
+    // realloc not implemented
+    return NULL;
+}
+
+void z_free(void *ptr) {
+        tx_byte_release(ptr);
+}
+
+#if Z_FEATURE_MULTI_THREAD == 1
+
+
+/*------------------ Thread ------------------*/
+z_result_t _z_task_init(_z_task_t *task, z_task_attr_t *attr, void *(*fun)(void *), void *arg) {
+
+	_Z_DEBUG("Creating a new task!");
+
+	UINT status = tx_thread_create(&(task->threadx_thread), "ztask", (VOID (*)(ULONG))fun, (ULONG)arg,
+			task->threadx_stack, Z_TASK_STACK_SIZE,
+			Z_TASK_PRIORITY,Z_TASK_PREEMPT_THRESHOLD,Z_TASK_TIME_SLICE,
+			TX_AUTO_START);
+
+	if (status != TX_SUCCESS)
+		return _Z_ERR_GENERIC;
+
+
+    return _Z_RES_OK;
+}
+
+z_result_t _z_task_join(_z_task_t *task) {
+
+	while(1)
+	{
+		UINT state;
+		UINT status = tx_thread_info_get(&(task->threadx_thread), NULL, &state, NULL, NULL, NULL, NULL, NULL, NULL);
+		if (status !=TX_SUCCESS)
+			return _Z_ERR_GENERIC;
+
+		if ( (state==TX_COMPLETED) || (state==TX_TERMINATED) )
+			break;
+
+		tx_thread_sleep(1);
+	}
+
+    return _Z_RES_OK;
+}
+
+z_result_t _z_task_detach(_z_task_t *task) {
+    // Not implemented
+    return _Z_ERR_GENERIC;
+}
+
+z_result_t _z_task_cancel(_z_task_t *task) {
+    // Not implemented
+    return _Z_ERR_GENERIC;
+}
+
+void _z_task_exit(void) { // NEW with new vesion
+    // Not implemented
+}
+
+void _z_task_free(_z_task_t **task) {
+    z_free(*task);
+    *task = NULL;
+}
+
+/*------------------ Mutex ------------------*/
+z_result_t _z_mutex_init(_z_mutex_t *m) {
+	UINT status = tx_mutex_create(m, TX_NULL, TX_INHERIT);
+	if (status == TX_MUTEX_ERROR){
+	    // zenoh-pico reuses mutex if zenoh_init() fails.
+	    status = tx_mutex_delete(m);
+	    if (status == TX_SUCCESS){
+	        status = tx_mutex_create(m, TX_NULL, TX_INHERIT);
+	    }
+	}
+    return (status == TX_SUCCESS) ? 0 : -1;
+}
+z_result_t _z_mutex_drop(_z_mutex_t *m) {
+	UINT status = tx_mutex_delete(m);
+	return (status == TX_SUCCESS) ? 0 : -1;
+}
+z_result_t _z_mutex_lock(_z_mutex_t *m) {
+	UINT status = tx_mutex_get(m,TX_WAIT_FOREVER);
+	return (status == TX_SUCCESS) ? 0 : -1;
+}
+z_result_t _z_mutex_try_lock(_z_mutex_t *m) {
+	UINT status = tx_mutex_get(m,TX_NO_WAIT); // Return immediately even if the mutex was not available
+	return (status == TX_SUCCESS) ? 0 : -1;
+}
+z_result_t _z_mutex_unlock(_z_mutex_t *m) {
+	UINT status = tx_mutex_put(m);
+	return (status == TX_SUCCESS) ? 0 : -1;
+}
+
+z_result_t _z_mutex_rec_init(_z_mutex_rec_t *m) { return _z_mutex_init(m); }
+z_result_t _z_mutex_rec_drop(_z_mutex_rec_t *m) { return _z_mutex_drop(m); }
+z_result_t _z_mutex_rec_lock(_z_mutex_rec_t *m) { return _z_mutex_lock(m); }
+z_result_t _z_mutex_rec_unlock(_z_mutex_rec_t *m) { return _z_mutex_unlock(m); }
+
+/*------------------ CondVar ------------------*/
+#define CONDVAR_MAX_WAITERS_COUNT 0xff;
+
+z_result_t _z_condvar_init(_z_condvar_t *cv) {
+    if (!cv) {
+        return _Z_ERR_GENERIC;
+    }
+    UINT m_status = tx_mutex_create(&cv->mutex, TX_NULL, TX_INHERIT);
+    UINT s_status = tx_semaphore_create(&cv->sem, TX_NULL, 0);
+    cv->waiters = 0;
+
+    if (m_status != TX_SUCCESS || s_status != TX_SUCCESS) {
+        return _Z_ERR_GENERIC;
+    }
+    return _Z_RES_OK;
+}
+
+z_result_t _z_condvar_drop(_z_condvar_t *cv) {
+    if (!cv) {
+        return _Z_ERR_GENERIC;
+    }
+    tx_mutex_delete(&cv->mutex);
+    tx_semaphore_delete(&cv->sem);
+    return _Z_RES_OK;
+}
+
+z_result_t _z_condvar_signal(_z_condvar_t *cv) {
+    if (!cv) {
+        return _Z_ERR_GENERIC;
+    }
+
+    tx_mutex_get(&cv->mutex, TX_WAIT_FOREVER);
+    if (cv->waiters > 0) {
+        tx_semaphore_put(&cv->sem);
+        cv->waiters--;
+    }
+    tx_mutex_put(&cv->mutex);
+
+    return _Z_RES_OK;
+}
+
+z_result_t _z_condvar_signal_all(_z_condvar_t *cv) {
+    if (!cv) {
+        return _Z_ERR_GENERIC;
+    }
+
+    tx_mutex_get(&cv->mutex, TX_WAIT_FOREVER);
+    while (cv->waiters > 0) {
+        tx_semaphore_put(&cv->sem);
+        cv->waiters--;
+    }
+    tx_mutex_put(&cv->mutex);
+
+    return _Z_RES_OK;
+}
+
+z_result_t _z_condvar_wait(_z_condvar_t *cv, _z_mutex_t *m) {
+    if (!cv || !m) {
+        return _Z_ERR_GENERIC;
+    }
+
+    tx_mutex_get(&cv->mutex, TX_WAIT_FOREVER);
+    cv->waiters++;
+    tx_mutex_put(&cv->mutex);
+
+    _z_mutex_unlock(m);
+    tx_semaphore_get(&cv->sem, TX_WAIT_FOREVER);
+
+    _z_mutex_lock(m);
+
+    return _Z_RES_OK;
+}
+
+z_result_t _z_condvar_wait_until(_z_condvar_t *cv, _z_mutex_t *m, const z_clock_t *abstime) {
+    if (!cv || !m) {
+        return _Z_ERR_GENERIC;
+    }
+
+    ULONG now = tx_time_get();
+    ULONG target_time = (abstime->tv_sec * 1000 + abstime->tv_nsec / 1000000) * (TX_TIMER_TICKS_PER_SECOND/1000);
+    ULONG block_duration = (target_time > now) ? (target_time - now) : 0;
+
+    tx_mutex_get(&cv->mutex, TX_WAIT_FOREVER);
+    cv->waiters++;
+    tx_mutex_put(&cv->mutex);
+
+    _z_mutex_unlock(m);
+
+    bool timed_out = tx_semaphore_get(&cv->sem, block_duration) != TX_SUCCESS;
+
+    _z_mutex_lock(m);
+
+    if (timed_out) {
+        tx_mutex_get(&cv->mutex, TX_WAIT_FOREVER);
+        cv->waiters--;
+        tx_mutex_put(&cv->mutex);
+        return Z_ETIMEDOUT;
+    }
+
+    return _Z_RES_OK;
+}
+#endif  // Z_MULTI_THREAD == 1
+
+/*------------------ Sleep ------------------*/
+z_result_t z_sleep_us(size_t time) {
+	tx_thread_sleep(time*TX_TIMER_TICKS_PER_SECOND/1000000);
+    return 0;
+}
+
+z_result_t z_sleep_ms(size_t time) {
+	tx_thread_sleep(time*TX_TIMER_TICKS_PER_SECOND/1000);
+    return 0;
+}
+
+z_result_t z_sleep_s(size_t time) {
+	tx_thread_sleep(time*TX_TIMER_TICKS_PER_SECOND);
+    return 0;
+}
+
+/*------------------ Clock ------------------*/
+
+void __z_clock_gettime(z_clock_t* ts) {
+    uint64_t ms = tx_time_get() * (TX_TIMER_TICKS_PER_SECOND/1000);
+    ts->tv_sec = ms / (uint64_t)1000;
+    ts->tv_nsec = (ms % (uint64_t)1000) * (uint64_t)1000;
+}
+
+z_clock_t z_clock_now(void) {
+    z_clock_t now;
+    __z_clock_gettime(&now);
+    return now;
+}
+
+unsigned long z_clock_elapsed_us(z_clock_t* instant) {
+    z_clock_t now;
+    __z_clock_gettime(&now);
+
+    unsigned long elapsed = (1000000 * (now.tv_sec - instant->tv_sec) + (now.tv_nsec - instant->tv_nsec) / 1000);
+    return elapsed;
+}
+
+unsigned long z_clock_elapsed_ms(z_clock_t* instant) {
+    z_clock_t now;
+    __z_clock_gettime(&now);
+
+    unsigned long elapsed = (1000 * (now.tv_sec - instant->tv_sec) + (now.tv_nsec - instant->tv_nsec) / 1000000);
+    return elapsed;
+}
+
+unsigned long z_clock_elapsed_s(z_clock_t* instant) {
+    z_clock_t now;
+    __z_clock_gettime(&now);
+
+    unsigned long elapsed = now.tv_sec - instant->tv_sec;
+    return elapsed;
+}
+
+void z_clock_advance_us(z_clock_t* clock, unsigned long duration) {
+    clock->tv_sec += duration / 1000000;
+    clock->tv_nsec += (duration % 1000000) * 1000;
+
+    if (clock->tv_nsec >= 1000000000) {
+        clock->tv_sec += 1;
+        clock->tv_nsec -= 1000000000;
+    }
+}
+
+void z_clock_advance_ms(z_clock_t* clock, unsigned long duration) {
+    clock->tv_sec += duration / 1000;
+    clock->tv_nsec += (duration % 1000) * 1000000;
+
+    if (clock->tv_nsec >= 1000000000) {
+        clock->tv_sec += 1;
+        clock->tv_nsec -= 1000000000;
+    }
+}
+
+void z_clock_advance_s(z_clock_t* clock, unsigned long duration) { clock->tv_sec += duration; }
+
+/*------------------ Time ------------------*/
+z_time_t z_time_now(void)  { return tx_time_get(); }
+
+const char *z_time_now_as_str(char *const buf, unsigned long buflen) {
+    snprintf(buf, buflen, "%lu", z_time_now());
+    return buf;
+}
+
+unsigned long z_time_elapsed_us(z_time_t *time) {
+	return (tx_time_get()-*time)*1000000ULL/TX_TIMER_TICKS_PER_SECOND;
+}
+
+unsigned long z_time_elapsed_ms(z_time_t *time) {
+	return (tx_time_get()-*time)*1000ULL/TX_TIMER_TICKS_PER_SECOND;
+}
+
+unsigned long z_time_elapsed_s(z_time_t *time) {
+	return (tx_time_get()-*time)*TX_TIMER_TICKS_PER_SECOND;
+}
+
+z_result_t _z_get_time_since_epoch(_z_time_since_epoch *t) {
+	ULONG64 time_ns = tx_time_get()*1000000000ULL/TX_TIMER_TICKS_PER_SECOND;
+
+	t->secs = time_ns/1000000000ULL;
+	t->nanos = time_ns%1000000000ULL;
+
+    return _Z_RES_OK;
+}
