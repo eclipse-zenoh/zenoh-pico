f2b820598d8218a45aa250a246f44a493d1e6020
Denis Biryukov <denis.biryukov@zettascale.tech>
2025-12-03 15:51:51 +0100

    lazily copy data for local messages

--- src/net/query.c
+++ src/net/query.c
@@ -28,24 +28,25 @@ static void _z_query_clear_inner(_z_query_t *q) {
     _z_session_weak_drop(&q->_zn);
 }
 
+z_result_t _z_session_send_reply_final(_z_session_t *session, uint32_t query_id, bool is_local) {
+    if (is_local) {
+        return _z_session_deliver_reply_final_locally(session, query_id);
+    } else {
+        _z_zenoh_message_t z_msg;
+        _z_n_msg_make_response_final(&z_msg, query_id);
+        z_result_t ret = _z_send_n_msg(session, &z_msg, Z_RELIABILITY_RELIABLE, Z_CONGESTION_CONTROL_BLOCK, NULL);
+        _z_msg_clear(&z_msg);
+        return ret;
+    }
+}
+
 z_result_t _z_query_send_reply_final(_z_query_t *q) {
-    // Try to upgrade session weak to rc
     _z_session_rc_t sess_rc = _z_session_weak_upgrade_if_open(&q->_zn);
     if (_Z_RC_IS_NULL(&sess_rc)) {
         _Z_ERROR_RETURN(_Z_ERR_TRANSPORT_TX_FAILED);
     }
 
-    _z_session_t *session = _Z_RC_IN_VAL(&sess_rc);
-    z_result_t ret = _Z_RES_OK;
-    if (q->_is_local) {
-        ret = _z_session_deliver_reply_final_locally(session, q->_request_id);
-    } else {
-        _z_zenoh_message_t z_msg;
-        _z_n_msg_make_response_final(&z_msg, q->_request_id);
-        ret = _z_send_n_msg(session, &z_msg, Z_RELIABILITY_RELIABLE, Z_CONGESTION_CONTROL_BLOCK, NULL);
-        _z_msg_clear(&z_msg);
-    }
-
+    z_result_t ret = _z_session_send_reply_final(_Z_RC_IN_VAL(&sess_rc), q->_request_id, q->_is_local);
     _z_session_rc_drop(&sess_rc);
     return ret;
 }
