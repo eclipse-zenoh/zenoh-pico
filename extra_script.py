047d2911f36f166b3f9eb51760aa33026bb3423e
Stefan Strogin <stefan@steils.org>
2026-03-23 23:07:53 +0000

    extra_script.py: rework to avoid adding exceptions for each new platform
    Closes: https://github.com/eclipse-zenoh/zenoh-pico/issues/818

--- extra_script.py
+++ extra_script.py
@@ -18,6 +18,19 @@ Import('env', 'projenv')
 
 SRC_FILTER = []
 CPPDEFINES = []
+ZP_PLATFORM = None
+
+BASE_SRC_FILTER = [
+    "+<*>",
+    "-<tests/>",
+    "-<example/>",
+    "-<system/>",
+    "+<system/common/>",
+]
+
+
+def _platform_src_filter(system_dir):
+    return BASE_SRC_FILTER + [f"+<{system_dir}/>"]
 
 FRAMEWORK = env.get("PIOFRAMEWORK")[0]
 PLATFORM = env.get("PIOPLATFORM")
@@ -29,20 +42,8 @@ if ZENOH_GENERIC == "1":
     BOARD = 'generic'
 
 if FRAMEWORK == 'zephyr':
-    SRC_FILTER = [
-        "+<*>",
-        "-<tests/>",
-        "-<example/>",
-        "-<system/arduino/>",
-        "-<system/emscripten/>",
-        "-<system/espidf/>",
-        "-<system/freertos/>",
-        "-<system/rpi_pico/>",
-        "-<system/mbed/>",
-        "-<system/unix/>",
-        "-<system/flipper/>",
-        "-<system/windows/>",
-    ]
+    SRC_FILTER = _platform_src_filter("system/zephyr")
+    ZP_PLATFORM = "zephyr"
     CPPDEFINES = [
         "ZENOH_ZEPHYR",
         "ZP_DEFAULT_STREAM_OPS=_z_tcp_zephyr_stream_ops",
@@ -53,21 +54,8 @@ if FRAMEWORK == 'zephyr':
 elif FRAMEWORK == 'arduino':
     PLATFORM = env.get("PIOPLATFORM")
     if PLATFORM == 'espressif32':
-        SRC_FILTER = [
-            "+<*>",
-            "-<tests/>",
-            "-<example/>",
-            "-<system/arduino/opencr>",
-            "-<system/emscripten/>",
-            "-<system/espidf>",
-            "-<system/freertos/>",
-            "-<system/rpi_pico/>",
-            "-<system/mbed/>",
-            "-<system/unix/>",
-            "-<system/flipper/>",
-            "-<system/windows/>",
-            "-<system/zephyr/>",
-        ]
+        SRC_FILTER = _platform_src_filter("system/arduino/esp32")
+        ZP_PLATFORM = "arduino_esp32"
         CPPDEFINES = [
             "ZENOH_ARDUINO_ESP32",
             "ZENOH_COMPILER_GCC",
@@ -79,21 +67,8 @@ elif FRAMEWORK == 'arduino':
     if PLATFORM == 'ststm32':
         BOARD = env.get("PIOENV")
         if BOARD == 'opencr':
-            SRC_FILTER = [
-                "+<*>",
-                "-<tests/>",
-                "-<example/>",
-                "-<system/arduino/esp32>",
-                "-<system/emscripten/>",
-                "-<system/espidf>",
-                "-<system/freertos/>",
-                "-<system/rpi_pico/>",
-                "-<system/mbed/>",
-                "-<system/unix/>",
-                "-<system/flipper/>",
-                "-<system/windows/>",
-                "-<system/zephyr/>",
-            ]
+            SRC_FILTER = _platform_src_filter("system/arduino/opencr")
+            ZP_PLATFORM = "opencr"
             CPPDEFINES = [
                 "ZENOH_ARDUINO_OPENCR",
                 "ZENOH_C_STANDARD=99",
@@ -103,20 +78,8 @@ elif FRAMEWORK == 'arduino':
             ]
 
 elif FRAMEWORK == 'espidf':
-    SRC_FILTER = [
-        "+<*>",
-        "-<tests/>",
-        "-<example/>",
-        "-<system/arduino/>",
-        "-<system/emscripten/>",
-        "-<system/freertos/>",
-        "-<system/rpi_pico/>",
-        "-<system/mbed/>",
-        "-<system/unix/>",
-        "-<system/flipper/>",
-        "-<system/windows/>",
-        "-<system/zephyr/>",
-    ]
+    SRC_FILTER = _platform_src_filter("system/espidf")
+    ZP_PLATFORM = "espidf"
     CPPDEFINES = [
         "ZENOH_ESPIDF",
         "ZP_DEFAULT_STREAM_OPS=_z_tcp_esp32_stream_ops",
@@ -125,20 +88,8 @@ elif FRAMEWORK == 'espidf':
     ]
 
 elif FRAMEWORK == 'mbed':
-    SRC_FILTER = [
-        "+<*>",
-        "-<tests/>",
-        "-<example/>",
-        "-<system/arduino/>",
-        "-<system/emscripten/>",
-        "-<system/espidf/>",
-        "-<system/freertos/>",
-        "-<system/rpi_pico/>",
-        "-<system/unix/>",
-        "-<system/flipper/>",
-        "-<system/windows/>",
-        "-<system/zephyr/>",
-    ]
+    SRC_FILTER = _platform_src_filter("system/mbed")
+    ZP_PLATFORM = "mbed"
     CPPDEFINES = [
         "ZENOH_MBED",
         "ZENOH_C_STANDARD=99",
@@ -147,7 +98,7 @@ elif FRAMEWORK == 'mbed':
         "ZP_DEFAULT_RAWIO_OPS=_z_uart_mbed_rawio_ops",
     ]
 elif FRAMEWORK == 'generic':
-    SRC_FILTER = ["+<*>", "-<tests/>", "-<example/>", "-<system/*>", "+<system/common>"]
+    SRC_FILTER = BASE_SRC_FILTER
     CPPDEFINES = ["ZENOH_GENERIC"]
 
 env.Append(SRC_FILTER=SRC_FILTER)
@@ -169,11 +120,23 @@ default_args = ["-DFRAG_MAX_SIZE=4096", "-DBATCH_UNICAST_SIZE=2048", "-DBATCH_MU
 cmake_extra_args = env.BoardConfig().get("build.cmake_extra_args", "")
 cmake_extra_args_list = cmake_extra_args.split()
 
+
+def _has_cmake_arg(name):
+    prefix = f"{name}="
+    return any(arg.startswith(prefix) for arg in cmake_extra_args_list)
+
+
+if (
+    ZP_PLATFORM is not None
+    and not _has_cmake_arg("-DZP_PLATFORM")
+    and not _has_cmake_arg("-DZP_SYSTEM_LAYER")
+):
+    cmake_extra_args_list.append(f"-DZP_PLATFORM={ZP_PLATFORM}")
+
 # Add default value if needed
-args_set = set(cmake_extra_args_list)
 for default in default_args:
     arg_name = default.split('=')[0]
-    if not any(arg.startswith(arg_name) for arg in args_set):
+    if not any(arg.startswith(arg_name) for arg in cmake_extra_args_list):
         cmake_extra_args_list.append(default)
 
 # Define the source and binary directories
